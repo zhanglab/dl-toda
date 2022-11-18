53bafca64fd0487c1321850d898256c25d6c96d6
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2022-11-17 22:18:50 -0500

    updated input filename

--- vis_scripts/metrics.R
+++ vis_scripts/metrics.R
@@ -2,13 +2,14 @@ library(ggplot2)
 
 args = commandArgs(trailingOnly = TRUE)
 input_dir = args[1]
+unclassified_option = args[2]
 
 # load and parse data
 values <- c()
 type <- c()
 group <- c()
-for(r in c("species", "genus", "family", "order", "class", "phylum")){
-  data <- read.csv(file.path(paste(input_dir, '/', r, "-metrics-wozeros.tsv", sep="")), sep='\t', header=TRUE)
+for(r in c("phylum", "class", "order", "family", "genus", "species")){
+  data <- read.csv(file.path(paste(input_dir, '/', r, "-metrics-", unclassified_option, ".tsv", sep="")), sep='\t', header=TRUE)
   data <- head(data, -4)
   values <- c(values, as.double(data$recall), as.double(data$precision), as.double(data$F1))
   type <- c(type, rep("Recall",length(data$recall)), rep("Precision",length(data$precision)), rep("F1-score",length(data$F1)))
@@ -20,7 +21,7 @@ df <- data.frame(group,type,values)
 df$group <- factor(df$group, levels = unique(df$group))
 
 # create facet plots
-tiff(file.path(input_dir, "metrics-plot.tiff", sep=""), units="px", width=3500, height=3500, res=300)
+tiff(file.path(paste(input_dir, "/", unclassified_option, "-metrics-plot.tiff", sep="")), units="px", width=3500, height=3500, res=300)
 bp <- ggplot(df, aes(y=values, fill=group)) + geom_boxplot(aes(fill=type)) + ylab("Precision/Recall/F1-score") + xlab("") +
   scale_fill_manual(values=c("#202020", "#606060", "#A0A0A0"), name="") + theme(panel.spacing = unit(1, "lines"), axis.text=element_text(size = 25), text=element_text(size = 28), panel.border=element_blank(), axis.ticks.x=element_blank(), axis.text.x=element_blank(), axis.title.x=element_blank(),
   axis.title.y=element_text(vjust=3, margin = margin(t = 0, r = 0, b = 0, l = 18)), legend.position = "bottom", legend.key.size =  unit(0.5, "in")) +
