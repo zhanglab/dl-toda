23db95dd8006e32369f5a8c3d89e0b454e2707f7
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2022-12-04 17:04:14 -0500

    updated indexes of taxonomic ranks

--- dataprep_scripts/labels.py
+++ dataprep_scripts/labels.py
@@ -11,15 +11,15 @@ def get_labels(tax_file, rank_index, taxa2labels):
             outf.write(f'{v}\t{k}\n')
 
     # create json dictionary
-    labels2taxa = {v: k.split(';')[rank_index-1] for k, v in taxa2labels.items()}
+    labels2taxa = {v: k.split(';')[rank_index] for k, v in taxa2labels.items()}
     with open('labels.json', 'w') as f:
         json.dump(labels2taxa, f)
 
-def get_genomes(tax_file):
+def get_genomes(tax_file, rank_index):
     genomes = defaultdict(list) # key = taxon, value = list of genomes
     with open(tax_file, 'r') as inf:
         for line in inf:
-            genomes[";".join(line.rstrip().split('\t')[1:])].append(line.rstrip().split('\t')[0])
+            genomes[";".join(line.rstrip().split('\t')[rank_index+1:])].append(line.rstrip().split('\t')[0])
     # create dictionary mapping labels to taxa
     taxa = list(set(genomes.keys()))
     taxa2labels = dict(zip(taxa,list(range(len(taxa))))) # key = taxon, value = label
@@ -27,6 +27,7 @@ def get_genomes(tax_file):
     out_train = open('training_genomes.tsv', 'w')
     out_test = open('testing_genomes.tsv', 'w')
     for k, v in genomes.items():
+        print(k, len(v))
         if len(v) > 1:
             random.shuffle(v)
             num_train_genomes = math.ceil(0.7*len(v))
@@ -44,10 +45,10 @@ def main():
     tax_file = sys.argv[1] # tsv file with genome and their assigned gtdb or ncbi taxonomy
     rank = sys.argv[2] # rank
 
-    ranks = {'species': 1, 'genus': 2, 'family': 3, 'order': 4, 'class': 5, 'phylum': 6}
+    ranks = {'species': 0, 'genus': 1, 'family': 2, 'order': 3, 'class': 4, 'phylum': 5}
 
     # get genomes for training and testing
-    taxa2labels = get_genomes(tax_file)
+    taxa2labels = get_genomes(tax_file, ranks[rank])
 
     # create dictionary mapping labels to taxa at given rank
     get_labels(tax_file, ranks[rank], taxa2labels)
