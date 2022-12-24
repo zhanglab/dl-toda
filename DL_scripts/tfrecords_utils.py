e4d32d1582aa6a48968734b89865e300517df1b3
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2022-12-24 11:30:41 -0500

    added print statement

--- DL_scripts/tfrecords_utils.py
+++ DL_scripts/tfrecords_utils.py
@@ -66,5 +66,6 @@ def get_kmer_arr(args, read):
     if len(list_kmers) < args.kmer_vector_length:
         # pad list of kmers with 0s to the right
         list_kmers = list_kmers + [0] * (args.kmer_vector_length - len(list_kmers))
-    print(f'{len(list_kmers)}\t{args.kmer_vector_length}\t{len(read)}')
+    if len(list_kmers) == 240:
+        print(f'{len(list_kmers)}\t{args.kmer_vector_length}\t{len(read)}\t{read}')
     return np.array(list_kmers)
