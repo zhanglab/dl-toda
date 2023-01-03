ff812561e31ccfe0c660371158821a13bbf9cddb
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2023-01-03 12:23:40 -0500

    modified binning to be performed per process

--- meta_scripts/taxa_profiling.py
+++ meta_scripts/taxa_profiling.py
@@ -19,20 +19,22 @@ def load_reads(args):
     args.reads = {rec.split('\n')[0]: rec for rec in records}
     print(len(records), len(args.reads))
 
-def parse_data(data, args, results, process_id):
-    process_results = defaultdict(int)
-    for line in data:
-        taxon = args.dl_toda_taxonomy[int(line.split('\t')[2])][args.ranks[args.rank]]
-        prob_score = float(line.split('\t')[3])
-        label = int(line.split('\t')[2])
-        if prob_score > args.cutoff:
-            process_results[label] += 1
-            if args.binning:
-                fq_filename = os.path.join(args.output_dir, 'tmp', f'{label}-{process_id}-tmp.fq') if args.rank == 'species' else os.path.join(args.output_dir, 'tmp', f'{taxon}-{process_id}-tmp.fq')
-                with open(fq_filename, 'a') as out_fq:
-                    out_fq.write(args.reads[str(line.split('\t')[0])])
+def parse_data(taxa, data, args, process_id):
+    out_filename = os.path.join(args.output_dir, '-'.join(args.dl_toda_output.split('/')[-1].split('-')[:-1]) + f'-cutoff-{args.cutoff}-{process_id}-out.tsv')
+    out_f = open(out_filename, 'w')
+    for t in taxa:
+        # get label(s)
+        l = [k for k, v in args.dl_toda_taxonomy.items() if v == t]
+        # get reads
+        t_reads_id = [v[0] for k, v in data.items() if k in l and v[2] > args.cutoff]
+        # write tax profile to output file
+        out_f.write(f'{t}\t{len(reads)}\n')
+        if args.binning:
+            t_reads = [args.reads[r] for r in t_reads_id]
+            fq_filename = os.path.join(args.output_dir, f'{process_id}', f'{l[0]}-bin.fq') if args.rank == 'species' else os.path.join(args.output_dir, f'{process_id}', f'{t.split(";")[0]}-bin.fq')
+            with open(fq_filename, 'a') as out_fq:
+                out_fq.write(''.join(t_reads))
 
-    results[process_id]= process_results
 
 
 if __name__ == "__main__":
@@ -57,15 +59,6 @@ if __name__ == "__main__":
     elif args.tax_db =='gtdb':
         index = 1
 
-    # update and create output directory
-    args.output_dir = os.path.join(args.output_dir, '-'.join(args.dl_toda_output.split('/')[-1].split('-')[:-1]), f'cutoff-{args.cutoff}')
-    if not os.path.exists(args.output_dir):
-        os.makedirs(args.output_dir)
-        os.makedirs(os.path.join(args.output_dir, 'tmp'))
-
-    # define name of output file with taxonomic profiles
-    args.output_file = os.path.join(args.output_dir, '-'.join(args.dl_toda_output.split('/')[-1].split('-')[:-1]) + f'-cutoff-{args.cutoff}-out.tsv')
-
     if args.binning:
         # load reads
         load_reads(args)
@@ -75,41 +68,56 @@ if __name__ == "__main__":
     with open('/'.join(os.path.dirname(os.path.abspath(__file__)).split('/')[:-1]) + '/data/dl_toda_taxonomy.tsv', 'r') as in_f:
         for line in in_f:
             line = line.rstrip().split('\t')
-            args.dl_toda_taxonomy[int(line[0])] = line[index].split(';')
+            args.dl_toda_taxonomy[int(line[0])] = ';'.join(line[index].split(';')[args.ranks[args.rank]:])
+    taxa = list(set(args.dl_toda_taxonomy.values()))
 
-    # split data amongst processes
+    # update and create output directory
+    args.output_dir = os.path.join(args.output_dir, '-'.join(args.dl_toda_output.split('/')[-1].split('-')[:-1]), f'cutoff-{args.cutoff}')
+    if not os.path.exists(args.output_dir):
+        for i in range(args.processes):
+            os.makedirs(os.path.join(args.output_dir, f'{i}'))
+
+    # define name of output file with taxonomic profiles
+    # args.output_file = os.path.join(args.output_dir, '-'.join(args.dl_toda_output.split('/')[-1].split('-')[:-1]) + f'-cutoff-{args.cutoff}-out.tsv')
+
+
+    # split taxa amongst processes
+    chunk_size = math.ceil(len(taxa)/args.processes)
+    taxa_groups = [taxa[i:i+chunk_size] for i in range(0,len(taxa),chunk_size)]
+    # load data
+    data = {}
     with open(args.dl_toda_output, 'r') as f:
-        content = f.readlines()
-    chunk_size = math.ceil(len(content)/args.processes)
-    data_split = [content[i:i+chunk_size] for i in range(0,len(content),chunk_size)]
+        for line in f:
+            data[line.rstrip().split('\t')[1]] = line.rstrip(.split('\t'))
+    # chunk_size = math.ceil(len(content)/args.processes)
+    # data_split = [content[i:i+chunk_size] for i in range(0,len(content),chunk_size)]
 
     with mp.Manager() as manager:
-        results = manager.dict()
-        processes = [mp.Process(target=parse_data, args=(data_split[i], args, results, i)) for i in range(len(data_split))]
+        processes = [mp.Process(target=parse_data, args=(taxa_groups[i], data, args, i)) for i in range(len(taxa_groups))]
         for p in processes:
             p.start()
         for p in processes:
             p.join()
 
         # create file with taxonomic profiles
-        with open(args.output_file, 'w') as out_f:
-            for k, v in args.dl_toda_taxonomy.items():
-                num_reads = 0
-                for process_id, process_results in results.items():
-                    for label, read_count in process_results.items():
-                        if label == k:
-                            num_reads += read_count
-                taxonomy = '\t'.join(v)
-                out_f.write(f'{taxonomy}\t{num_reads}\n')
+        # with open(args.output_file, 'w') as out_f:
+        #     for k, v in args.dl_toda_taxonomy.items():
+        #         num_reads = 0
+        #         for process_id, process_results in results.items():
+        #             for label, read_count in process_results.items():
+        #                 if label == k:
+        #                     num_reads += read_count
+        #         taxonomy = '\t'.join(v)
+        #         out_f.write(f'{taxonomy}\t{num_reads}\n')
 
-                if args.binning:
-                    # combine fastq files
-                    prefix = k if args.rank == 'species' else v[args.ranks[args.rank]]
-                    fq_files = sorted(glob.glob(os.path.join(args.output_dir, 'tmp', f'{prefix}-*-tmp.fq')))
-                    with open(os.path.join(args.output_dir, f'{prefix}-bin.fq'), 'w') as out_fq:
-                        for fq in fq_files:
-                            with open(fq, 'r') as in_fq:
-                                out_fq.write(in_fq.read())
-        if args.binning:
-            # remove tmp fq files
-            shutil.rmtree(os.path.join(args.output_dir, 'tmp'))
+                # if args.binning:
+                #     # combine fastq files
+                #     prefix = k if args.rank == 'species' else v[args.ranks[args.rank]]
+                #     fq_files = sorted(glob.glob(os.path.join(args.output_dir, 'tmp', f'{prefix}-*-tmp.fq')))
+                #     with open(os.path.join(args.output_dir, f'{prefix}-bin.fq'), 'w') as out_fq:
+                #         for fq in fq_files:
+                #             with open(fq, 'r') as in_fq:
+                #                 out_fq.write(in_fq.read())
+        # if args.binning:
+        #     # remove tmp fq files
+        #     shutil.rmtree(os.path.join(args.output_dir, 'tmp'))
