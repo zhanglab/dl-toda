830affc20e28d620ac7b87937d217cb1d7efe10c
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2023-01-20 09:16:16 -0500

    modified filename

--- vis_scripts/Figure3_learning_curves.R
+++ vis_scripts/Figure3_learning_curves.R
@@ -1,4 +1,5 @@
 library(ggplot2)
+library(ggpubr)
 
 args = commandArgs(trailingOnly = TRUE)
 input_dir = args[1]
@@ -16,18 +17,21 @@ x_axis_ticks = seq(from = 1, to = num_epochs, by = 1)
 linetypes <- c("Training" = "solid", "Validation" = "dashed")
 
 # plot training and validation accuracy
-tiff("accuracy.tiff", units="px", width=3500, height=3500, res=300)
-ggplot() + geom_line(data=training_data, aes(x=V2, y=V5, linetype="Training"), size=1) +
+#tiff("accuracy.tiff", units="in", width=4.5, height=4, res=300)
+plot1 <- ggplot() + geom_line(data=training_data, aes(x=V2, y=V5, linetype="Training"), size=1) +
   geom_line(data=validation_data, aes(x=V2, y=V4, linetype="Validation"), size=1) + geom_vline(xintercept=14,lwd=2,colour="black") +
   xlab("Epoch") + ylab("Accuracy") + theme(axis.text=element_text(size = 25), text=element_text(size = 28), legend.title=element_blank(), legend.position = "bottom", legend.key.size =  unit(0.5, "in")) + coord_cartesian(ylim=c(min(validation_data$V4,training_data$V5),1)) +
   scale_x_continuous(breaks=x_axis_breaks, labels=x_axis_ticks) + scale_linetype_manual(values = linetypes)
-dev.off()
+#dev.off()
 
 # plot training and validation loss
-tiff("loss.tiff", units="px", width=3500, height=3500, res=300)
-ggplot() + geom_line(data=training_data, aes(x=V2, y=V4, linetype="Training"), size=1) +
+#tiff("loss.tiff", units="in", width=4.5, height=4, res=300)
+plot2 <- ggplot() + geom_line(data=training_data, aes(x=V2, y=V4, linetype="Training"), size=1) +
   geom_line(data=validation_data, aes(x=V2, y=V3, linetype="Validation"), size=1) + geom_vline(xintercept=14,lwd=2,colour="black") +
   xlab("Epoch") + ylab("Loss") + theme(axis.text=element_text(size = 25), text=element_text(size = 28), legend.title=element_blank(), legend.position = "bottom", legend.key.size =  unit(0.5, "in")) + coord_cartesian(ylim=c(min(validation_data$V3,training_data$V4),max(validation_data$V3,training_data$V4)+0.5)) +
   scale_x_continuous(breaks=x_axis_breaks, labels=x_axis_ticks) + scale_linetype_manual(values = linetypes)
-dev.off()
+#dev.off()
 
+tiff("learning_curves.tiff", units="in", width=4.5, height=4, res=300)
+ggarrange(plot1, plot2, labels = c("A", "B"), nol=2, nrow=1)
+dev.off()
