a392d9293351e6dc5710061d9bee2de99f5c36ce
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2023-02-01 20:47:49 -0500

    modified x axis to coverage

--- vis_scripts/Figure1_hist.R
+++ vis_scripts/Figure1_hist.R
@@ -12,14 +12,14 @@ train_data <- read.csv(train_input_file, sep='\t', header=FALSE)
 test_data <- read.csv(test_input_file, sep='\t', header=FALSE)
 
 
-cat("training - ", "median: ", median(train_data$V2), " min: ", min(train_data$V2), " max: ", max(train_data$V2), "\n")
-cat("testing - ", "median: ", median(train_data$V2), " min: ", min(train_data$V2), " max: ", max(train_data$V2), "\n")
+cat("training - ", "median: ", median(train_data$V4), " min: ", min(train_data$V4), " max: ", max(train_data$V4), "\n")
+cat("testing - ", "median: ", median(train_data$V4), " min: ", min(train_data$V4), " max: ", max(train_data$V4), "\n")
 
 
 # plot histogram of read count per species for training and testing datasets
-plot1 <- ggplot(train_data, aes(x=log(V2))) + geom_histogram() +
-  ylab("Number of species") + 
-  xlab("Number of training reads per species (natural log)") +
+plot1 <- ggplot(train_data, aes(x=log(V4))) + geom_histogram() +
+  ylab("Number of species") +
+  xlab("Coverage of training genomes per species (natural log)") +
   theme(axis.title = element_text(size = 12),
         axis.text = element_text(size = 12),
         panel.background = element_rect(fill='white'),
@@ -29,9 +29,9 @@ plot1 <- ggplot(train_data, aes(x=log(V2))) + geom_histogram() +
   scale_x_continuous(breaks = pretty_breaks())
 
 
-plot2 <- ggplot(test_data, aes(x=log(V2))) + geom_histogram() +
-  ylab("Number of species") + 
-  xlab("Number of testing reads per species (natural log)") +
+plot2 <- ggplot(test_data, aes(x=log(V4))) + geom_histogram() +
+  ylab("Number of species") +
+  xlab("Coverage of testing genomes per species (natural log)") +
   theme(axis.title = element_text(size = 12),
         axis.text = element_text(size = 12),
         panel.background = element_rect(fill='white'),
@@ -42,6 +42,6 @@ plot2 <- ggplot(test_data, aes(x=log(V2))) + geom_histogram() +
 
 
 plot_list <- c(plot1, plot2)
-tiff(file.path(paste(output_dir, 'train_test_hist.tiff', sep = "/")), units="in", width=8.5, height=6, res=300)
+tiff(file.path(paste(output_dir, 'train_test_hist.tiff', sep = "/")), units="in", width=9.5, height=6, res=300)
 ggarrange(plot1, plot2, labels = c("A", "B"), ncol=2, nrow=1)
 dev.off()
