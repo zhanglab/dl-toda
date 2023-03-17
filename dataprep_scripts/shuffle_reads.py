b8a719472f2d94110a7893d4e85b40bf805c51b5
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2023-03-17 18:58:18 +0100

    used biopython to load reads

--- dataprep_scripts/shuffle_reads.py
+++ dataprep_scripts/shuffle_reads.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import random
+from Bio import SeqIO
 
 
 if __name__ == "__main__":
@@ -9,11 +10,9 @@ if __name__ == "__main__":
 
     output_file = os.path.join(output_dir, input_fq.split('/')[-1][:-3] + '-shuffled.fq')
     with open(output_file, 'w') as out_f:
-        with open(input_fq, 'r') as f:
-            content = f.readlines()
-            reads = [''.join(content[j:j+4]) for j in range(0, len(content), 4)]
-            random.shuffle(reads)
-            out_f.write(''.join(reads))
+        reads = list(SeqIO.parse(input_fq, "fastq"))
+        random.shuffle(reads)
+        out_f.write(''.join(reads))
 
 
 
