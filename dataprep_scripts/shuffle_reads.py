7da7cbbf806eece48b022d5e66eddbfeb214b7be
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2023-03-17 18:51:41 +0100

    specify mode for output file

--- dataprep_scripts/shuffle_reads.py
+++ dataprep_scripts/shuffle_reads.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     output_dir = sys.argv[2]
 
     output_file = os.path.join(output_dir, input_fq.split('/')[-1][:-3] + '-shuffled.fq')
-    with open(output_file) as out_f:
+    with open(output_file, 'w') as out_f:
         with open(input_fq, 'r') as f:
             content = f.readlines()
             reads = [''.join(content[j:j+4]) for j in range(0, len(content), 4)]
