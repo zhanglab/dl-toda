78eb0ef3f22e2889fc06c5cf97ea0bc75d27e1fa
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2023-05-02 08:51:12 -0400

    create file

---
+++ dataprep_scripts/rank_data.py
@@ -0,0 +1,58 @@
+import json
+import os
+import sys
+from collections import defaultdict
+import argparse
+from labels import get_labels
+
+
+def map_taxa2labels(args, ranks):
+    # define index of taxonomic database
+    index = 1 if args.tax_db == 'gtdb' else 2
+
+    path_dl_toda_tax = '/'.join(
+        os.path.dirname(os.path.abspath(__file__)).split('/')[:-1]) + '/data/dl_toda_taxonomy.tsv'
+
+    taxa = set()
+    with open(path_dl_toda_tax, 'r') as in_f:
+        content = in_f.readlines()
+        # get taxa
+        gtdb_taxonomy = {line[index].split(';')[ranks[args.rank]]: ';'.join(line[1].split(';')[ranks[args.rank]:6]) for
+                         line in content}
+        ncbi_taxonomy = {line[index].split(';')[ranks[args.rank]]: ';'.join(line[2].split(';')[ranks[args.rank]:6]) for
+                         line in content}
+        print(gtdb_taxonomy)
+        print(ncbi_taxonomy)
+        taxa2labels = dict(zip(gtdb_taxonomy.keys(), list(range(len(taxa)))))  # key = taxon, value = label
+        print(taxa2labels)
+        # create dictionary mapping labels to taxa
+        get_labels(ranks[args.rank], args.rank, taxa2labels)
+        # create file mapping species labels to given rank labels
+        with open(f'species_to_{args.rank}', 'w') as out_f:
+            for line in content:
+                line = line.rstrip().split('\t')
+                taxon = line[index].split(';')[ranks[args.rank]]
+                out_f.write(f'{line[0]}\t{taxa2labels[taxon]}\n')
+        # create file mapping rank labels to gtdb and ncbi taxonomy
+        with open(f'dl_toda_taxonomy_{args.rank}.tsv') as out_f:
+            for k, v in gtdb_taxonomy.items():
+                out_f.write(f'{taxa2labels[k]}\t{v}\t{ncbi_taxonomy[k]}\n')
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--tax_file', type=str, help='')
+    parser.add_argument('--rank', type=str, help='taxonomic rank',
+                        choices=['species', 'genus', 'family', 'order', 'class', 'phylum'])
+    parser.add_argument('--tax_db', type=str, help='taxonomic database',
+                        choices=['gtdb', 'ncbi'])
+    parser.add_argument('--dl_toda_tax', help='create dl_toda_taxonomy.tsv file', action='store_true')
+    args = parser.parse_args()
+
+    ranks = {'species': 0, 'genus': 1, 'family': 2, 'order': 3, 'class': 4, 'phylum': 5}
+
+    map_taxa2labels(args, ranks)
+
+
+if __name__ == "__main__":
+    main()
