03390c4c64b002f16115da64df0d828dffd0a11d
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2023-05-07 10:38:13 -0400

    added options for new DNA representation

--- DL_scripts/VGG16.py
+++ DL_scripts/VGG16.py
@@ -3,15 +3,16 @@ import os
 
 
 def VGG16(args, VECTOR_SIZE, EMBEDDING_SIZE, NUM_CLASSES, VOCAB_SIZE, DROPOUT_RATE):
-    n_rows = args.n_rows
-    n_cols = args.n_cols
+
     read_input = tf.keras.layers.Input(shape=(VECTOR_SIZE), dtype='int32')
     x = read_input
-    x = tf.keras.layers.Reshape((n_rows, n_cols))(x)
+    if args.DNA_model:
+        x = tf.keras.layers.Reshape((args.n_rows, args.n_cols))(x)
     x = tf.keras.layers.Embedding(input_dim=VOCAB_SIZE + 1, output_dim=EMBEDDING_SIZE,
                                   embeddings_initializer=tf.keras.initializers.HeNormal(),
                                   input_length=VECTOR_SIZE, mask_zero=True, trainable=True, name='embedding')(x)
-    x = tf.keras.layers.Reshape((n_rows, n_cols * EMBEDDING_SIZE, 1))(x)  # output shape: (n_rows, n_cols*EMBEDDING_SIZE, 1)
+    if args.DNA_model:
+        x = tf.keras.layers.Reshape((args.n_rows, args.n_cols * EMBEDDING_SIZE, 1))(x)  # output shape: (n_rows, n_cols*EMBEDDING_SIZE, 1)
     x = tf.keras.layers.Conv2D(64, kernel_size=(3, 3), padding='same', name='conv_1')(x)
     x = tf.keras.layers.BatchNormalization(axis=1, momentum=0.99)(x)
     x = tf.keras.layers.Activation('relu')(x)
