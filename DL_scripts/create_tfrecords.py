9f874c15edc4ee7667fc748acb43c156a36c33ce
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2023-05-16 14:05:21 -0400

    added option to process reads as pairs

--- DL_scripts/create_tfrecords.py
+++ DL_scripts/create_tfrecords.py
@@ -55,55 +55,56 @@ def create_tfrecords(args):
     output_prefix = '.'.join(args.input_fastq.split('/')[-1].split('.')[0:-1])
     output_tfrec = os.path.join(args.output_dir, output_prefix + '.tfrec')
     bases = {'A': 2, 'T': 3, 'C': 4, 'G': 5}
-    count = 0
+    num_lines = 8 if args.pair else 4
+    # count = 0
     with tf.compat.v1.python_io.TFRecordWriter(output_tfrec) as writer:
         with open(args.input_fastq) as handle:
-            rec = ''
-            n_line = 0
-            for line in handle:
-                rec += line
-                n_line += 1
-                if n_line == 4:
-            # content = handle.readlines()
-            # reads = [''.join(content[j:j + 4]) for j in range(0, len(content), 4)]
-            # for count, rec in enumerate(reads, 1):
+            # rec = ''
+            # n_line = 0
+            # for line in handle:
+            #     rec += line
+            #     n_line += 1
+            #     if n_line == 4:
+            content = handle.readlines()
+            reads = [''.join(content[j:j + num_lines]) for j in range(0, len(content), num_lines)]
+            for count, rec in enumerate(reads, 1):
             # for count, rec in enumerate(SeqIO.parse(handle, 'fastq'), 1):
             #     read = str(rec.seq)
             #     read_id = rec.id
                 # label = int(read_id.split('|')[1])
-                    read = rec.split('\n')[1].rstrip()
-                    label = int(rec.split('\n')[0].rstrip().split('|')[1])
-                    # update label if necessary
-                    if args.update_labels:
-                        label = int(args.labels_mapping[str(label)])
-                    if args.DNA_model:
-                        list_bases = [bases[x] if x in bases else 1 for x in read]
-                        # update read length to match the max read length
-                        if len(list_bases) < args.read_length:
-                            # pad list of bases with 0s to the right
-                            list_bases = list_bases + [0] * (args.read_length - len(list_bases))
-                        kmer_array = np.array(list_bases)
-                    else:
-                        kmer_array = get_kmer_arr(args, read)
+                read_id = rec.split('\n')[0].rstrip()
+                read = rec.split('\n')[1].rstrip() + rec.split('\n')[5].rstrip() if args.pair else rec.split('\n')[1].rstrip()
+                label = int(read_id.split('|')[1])
+                # update label if necessary
+                if args.update_labels:
+                    label = int(args.labels_mapping[str(label)])
+                if args.DNA_model:
+                    dna_array = [bases[x] if x in bases else 1 for x in read]
+                    # update read length to match the max read length
+                    if len(dna_array) < args.read_length:
+                        # pad list of bases with 0s to the right
+                        dna_array = dna_array + [0] * (args.read_length - len(dna_array))
+                else:
+                    dna_array = get_kmer_arr(args, read, read_id)
                     if args.no_label:
                         data = \
                             {
-                                'read': wrap_read(kmer_array),
+                                'read': wrap_read(np.array(dna_array)),
                             }
                     else:
                         data = \
                             {
-                                'read': wrap_read(kmer_array),
+                                'read': wrap_read(np.array(dna_array)),
                                 'label': wrap_label(label),
                             }
                     feature = tf.train.Features(feature=data)
                     example = tf.train.Example(features=feature)
                     serialized = example.SerializeToString()
                     writer.write(serialized)
-                    count += 1
+                    # count += 1
                     # initialize variables again
-                    n_line = 0
-                    rec = ''
+                    # n_line = 0
+                    # rec = ''
 
         with open(os.path.join(args.output_dir, output_prefix + '-read_count'), 'w') as f:
             f.write(f'{count}')
@@ -115,8 +116,9 @@ def main():
     parser.add_argument('--input_fastq', help="Path to the input fastq file")
     parser.add_argument('--output_dir', help="Path to the output directory")
     parser.add_argument('--vocab', help="Path to the vocabulary file")
-    parser.add_argument('--DNA_model', action='store_true', default=False)
-    parser.add_argument('--no_label', action='store_true', default=False)
+    parser.add_argument('--DNA_model', action='store_true', default=False, help="represent reads for DNA model")
+    parser.add_argument('--no_label', action='store_true', default=False, help="do not addd labels to tfrecords")
+    parser.add_argument('--pair', action='store_true', default=False, help="represent reads as pairs")
     parser.add_argument('--k_value', default=12, type=int, help="Size of k-mers")
     parser.add_argument('--update_labels', action='store_true', default=False, required=('--mapping_file' in sys.argv))
     parser.add_argument('--mapping_file', type=str, help='path to file mapping species labels to rank labels')
