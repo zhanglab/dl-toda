18bcccc4c32febb9edbc439a18a7accd55831978
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2023-05-27 05:28:56 -0400

    modify reading of fq file

--- DL_scripts/create_tfrecords.py
+++ DL_scripts/create_tfrecords.py
@@ -56,75 +56,75 @@ def create_tfrecords(args):
     output_tfrec = os.path.join(args.output_dir, output_prefix + '.tfrec')
     bases = {'A': 2, 'T': 3, 'C': 4, 'G': 5}
     num_lines = 8 if args.pair else 4
-    # count = 0
+    count = 0
     with tf.compat.v1.python_io.TFRecordWriter(output_tfrec) as writer:
         with open(args.input_fastq) as handle:
-            # rec = ''
-            # n_line = 0
-            # for line in handle:
-            #     rec += line
-            #     n_line += 1
-            #     if n_line == 4:
-            content = handle.readlines()
-            reads = [''.join(content[j:j + num_lines]) for j in range(0, len(content), num_lines)]
-            for count, rec in enumerate(reads, 1):
+            rec = ''
+            n_line = 0
+            for line in handle:
+                rec += line
+                n_line += 1
+                if n_line == num_lines:
+            # content = handle.readlines()
+            # reads = [''.join(content[j:j + num_lines]) for j in range(0, len(content), num_lines)]
+            # for count, rec in enumerate(reads, 1):
             # for count, rec in enumerate(SeqIO.parse(handle, 'fastq'), 1):
             #     read = str(rec.seq)
             #     read_id = rec.id
                 # label = int(read_id.split('|')[1])
-                read_id = rec.split('\n')[0].rstrip()
-                label = int(read_id.split('|')[1])
-                print(read_id, label)
-                # update label if necessary
-                if args.update_labels:
-                    label = int(args.labels_mapping[str(label)])
-                if args.pair:
-                    if args.DNA_model:
-                        fw_dna_array = [bases[x] if x in bases else 1 for x in rec.split('\n')[1].rstrip()]
-                        rv_dna_array = [bases[x] if x in bases else 1 for x in rec.split('\n')[5].rstrip()]
-                        # update read length to match the max read length
-                        if len(fw_dna_array) < args.read_length:
-                            # pad list of bases with 0s to the right
-                            fw_dna_array = fw_dna_array + [0] * (args.read_length - len(fw_dna_array))
-                            rv_dna_array = rv_dna_array + [0] * (args.read_length - len(rv_dna_array))
-                    else:
-                        fw_dna_array = get_kmer_arr(args, rec.split('\n')[1].rstrip())
-                        rv_dna_array = get_kmer_arr(args, rec.split('\n')[5].rstrip())
+                    read_id = rec.split('\n')[0].rstrip()
+                    label = int(read_id.split('|')[1])
+                    print(read_id, label)
+                    # update label if necessary
+                    if args.update_labels:
+                        label = int(args.labels_mapping[str(label)])
+                    if args.pair:
+                        if args.DNA_model:
+                            fw_dna_array = [bases[x] if x in bases else 1 for x in rec.split('\n')[1].rstrip()]
+                            rv_dna_array = [bases[x] if x in bases else 1 for x in rec.split('\n')[5].rstrip()]
+                            # update read length to match the max read length
+                            if len(fw_dna_array) < args.read_length:
+                                # pad list of bases with 0s to the right
+                                fw_dna_array = fw_dna_array + [0] * (args.read_length - len(fw_dna_array))
+                                rv_dna_array = rv_dna_array + [0] * (args.read_length - len(rv_dna_array))
+                        else:
+                            fw_dna_array = get_kmer_arr(args, rec.split('\n')[1].rstrip())
+                            rv_dna_array = get_kmer_arr(args, rec.split('\n')[5].rstrip())
 
-                    dna_array = fw_dna_array + rv_dna_array
-                    print(dna_array)
-                    # append insert size for kmers arrays as pairs of reads
-                    dna_array.append(int(args.dict_kmers[read_id.split('|')[3]]))
-                    print(dna_array)
-                else:
-                    if args.DNA_model:
-                        dna_array = [bases[x] if x in bases else 1 for x in rec.split('\n')[1].rstrip()]
-                        # update read length to match the max read length
-                        if len(dna_array) < args.read_length:
-                            # pad list of bases with 0s to the right
-                            dna_array = dna_array + [0] * (args.read_length - len(dna_array))
+                        dna_array = fw_dna_array + rv_dna_array
+                        print(dna_array)
+                        # append insert size for kmers arrays as pairs of reads
+                        dna_array.append(int(args.dict_kmers[read_id.split('|')[3]]))
+                        print(dna_array)
                     else:
-                        dna_array = get_kmer_arr(args, rec.split('\n')[1].rstrip())
-                if args.no_label:
-                    data = \
-                        {
-                            'read': wrap_read(np.array(dna_array)),
-                        }
-                else:
-                    data = \
-                        {
-                            'read': wrap_read(np.array(dna_array)),
-                            'label': wrap_label(label),
-                        }
-                feature = tf.train.Features(feature=data)
-                example = tf.train.Example(features=feature)
-                serialized = example.SerializeToString()
-                writer.write(serialized)
-                # count += 1
-                # initialize variables again
-                # n_line = 0
-                # rec = ''
-                break
+                        if args.DNA_model:
+                            dna_array = [bases[x] if x in bases else 1 for x in rec.split('\n')[1].rstrip()]
+                            # update read length to match the max read length
+                            if len(dna_array) < args.read_length:
+                                # pad list of bases with 0s to the right
+                                dna_array = dna_array + [0] * (args.read_length - len(dna_array))
+                        else:
+                            dna_array = get_kmer_arr(args, rec.split('\n')[1].rstrip())
+                    if args.no_label:
+                        data = \
+                            {
+                                'read': wrap_read(np.array(dna_array)),
+                            }
+                    else:
+                        data = \
+                            {
+                                'read': wrap_read(np.array(dna_array)),
+                                'label': wrap_label(label),
+                            }
+                    feature = tf.train.Features(feature=data)
+                    example = tf.train.Example(features=feature)
+                    serialized = example.SerializeToString()
+                    writer.write(serialized)
+                    count += 1
+                    # initialize variables again
+                    n_line = 0
+                    rec = ''
+                    break
 
         with open(os.path.join(args.output_dir, output_prefix + '-read_count'), 'w') as f:
             f.write(f'{count}')
