fcd4aa7a06cc291e4cbc26d39dd71a4e27846bc8
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2024-02-28 14:07:05 -0500

    import defaultdict

--- DL_scripts/tfrecords_bert_utils.py
+++ DL_scripts/tfrecords_bert_utils.py
@@ -10,7 +10,7 @@ def split_read(reads, read, r_index, process):
     # nsp_choice = True
     # define first segment
     segment_1 = read[:len(read)//2]
-    if nsp_choice == 'False':
+    if nsp_choice == False:
         nsp_label = 1 # 'IsNext' --> verified with bert code on sample text
         segment_2 = read[len(read)//2:]
     else:
