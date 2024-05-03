4c4efdd1656312d3ee8040988bbe6da081bb3d3c
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2024-05-03 12:35:22 -0400

    add calue for initial learning rate

--- DL_scripts/modeling.py
+++ DL_scripts/modeling.py
@@ -615,43 +615,32 @@ def training_step(data, train_accuracy, loss, opt, model, first_batch):
 
         # hidden_size = output_layer.shape[-1].value
         hidden_size = output_layer.shape[-1]
-        print(f'hidden_size: {hidden_size}')
 
-        output_weights = tf.Variable(name="output_weights")
+        output_weights = tf.compat.v1.get_variable(
+            "output_weights", [num_labels, hidden_size],
+            initializer=tf.keras.initializers.TruncatedNormal(stddev=0.02))
+        print(f'output_weights: {output_weights}')
 
-        # output_weights = tf.get_variable(
-        #     "output_weights", [num_labels, hidden_size],
-        #     initializer=tf.truncated_normal_initializer(stddev=0.02))
-        # print(f'output_weights: {output_weights}')
+        output_bias = tf.compat.v1.get_variable(
+            "output_bias", [num_labels], initializer=tf.zeros_initializer())
+        print(f'output_bias: {output_bias}')
 
-        # output_bias = tf.get_variable(
-        #     "output_bias", [num_labels], initializer=tf.zeros_initializer())
-        # print(f'output_bias: {output_bias}')
+        output_layer = tf.nn.dropout(output_layer, keep_prob=0.9)
 
-        # with tf.variable_scope("loss"):
-        #   if is_training:
-        #     # I.e., 0.1 dropout
-        #     output_layer = tf.nn.dropout(output_layer, keep_prob=0.9)
+        logits = tf.linalg.matmul(output_layer, output_weights, transpose_b=True)
+        logits = tf.nn.bias_add(logits, output_bias)
+        probabilities = tf.nn.softmax(logits, axis=-1)
+        log_probs = tf.nn.log_softmax(logits, axis=-1)
 
-        #   logits = tf.matmul(output_layer, output_weights, transpose_b=True)
-        #   logits = tf.nn.bias_add(logits, output_bias)
-        #   probabilities = tf.nn.softmax(logits, axis=-1)
-        #   log_probs = tf.nn.log_softmax(logits, axis=-1)
+        one_hot_labels = tf.one_hot(labels, depth=num_labels, dtype=tf.float32)
 
-        #   one_hot_labels = tf.one_hot(labels, depth=num_labels, dtype=tf.float32)
+        per_example_loss = -tf.reduce_sum(one_hot_labels * log_probs, axis=-1)
+        loss_value = tf.reduce_mean(per_example_loss)
 
-        #   per_example_loss = -tf.reduce_sum(one_hot_labels * log_probs, axis=-1)
-        #   loss = tf.reduce_mean(per_example_loss)
+        grads = tape.gradient(loss_value, model.trainable_variables)
+        opt.apply_gradients(zip(grads, model.trainable_variables))
 
-        #   return (loss, per_example_loss, logits, probabilities)
-
-
-        
-    #     # probs = model(reads, training=True)
-    #     # get the loss
-    #     loss_value = loss(labels, probs)
-    #     grads = tape.gradient(loss_value, model.trainable_variables)
-    #     opt.apply_gradients(zip(grads, model.trainable_variables))
+        print(loss_value)
 
 
     # #update training accuracy
