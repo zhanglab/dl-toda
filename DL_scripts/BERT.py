c1beb2264b11831335ac7cd1085c62e6aca5eed8
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2024-05-11 21:08:46 -0400

    modify position embeddings layer

--- DL_scripts/BERT.py
+++ DL_scripts/BERT.py
@@ -96,44 +96,13 @@ class BertConfig(object):
       return json.dumps(self.to_dict(), indent=2, sort_keys=True) + "\n"
 
 
-# def PositionalEncoding(config, seq_length, width):
-#     assert_op = tf.debugging.assert_less_equal(seq_length, config.max_position_embeddings)
-#     with tf.control_dependencies([assert_op]):
-#         weights_initializer = create_initializer(config.initializer_range)
-#         full_position_embeddings = tf.Variable(
-#             initial_value=weights_initializer(shape=[config.max_position_embeddings, config.hidden_size],dtype='float16'),
-#             name="position_embeddings")
-#         # Since the position embedding table is a learned variable, we create it
-#         # using a (long) sequence length `max_position_embeddings`. The actual
-#         # sequence length might be shorter than this, for faster training of
-#         # tasks that do not have long sequences.
-#         #
-#         # So `full_position_embeddings` is effectively an embedding table
-#         # for position [0, 1, 2, ..., max_position_embeddings-1], and the current
-#         # sequence has positions [0, 1, 2, ... seq_length-1], so we can just
-#         # perform a slice.
-#         position_embeddings = tf.slice(full_position_embeddings, [0, 0],
-#                                          [seq_length, -1])
-
-#         # Only the last two dimensions are relevant (`seq_length` and `width`), so
-#         # we broadcast among the first dimensions, which is typically just
-#         # the batch size.
-#         position_broadcast_shape = []
-#         num_dims = 3
-#         for _ in range(num_dims - 2):
-#             position_broadcast_shape.append(1)
-#         position_broadcast_shape.extend([seq_length, width])
-#         position_embeddings = tf.reshape(position_embeddings,
-#                                          position_broadcast_shape)
-#         return position_embeddings
-
-
-class PositionalEncoding(tf.keras.layers.Layer):
-    def __init__(self, config):
-        super().__init__()
-        self.seq_length = config.seq_length
-        self.width = config.hidden_size
-        self.weights_initializer = create_initializer(config.initializer_range)
+def PositionalEncoding(config):
+    assert_op = tf.debugging.assert_less_equal(config.seq_length, config.max_position_embeddings)
+    with tf.control_dependencies([assert_op]):
+        weights_initializer = create_initializer(config.initializer_range)
+        full_position_embeddings = tf.Variable(
+            initial_value=weights_initializer(shape=[config.max_position_embeddings, config.hidden_size],dtype='float16'),
+            name="position_embeddings")
         # Since the position embedding table is a learned variable, we create it
         # using a (long) sequence length `max_position_embeddings`. The actual
         # sequence length might be shorter than this, for faster training of
@@ -143,14 +112,9 @@ class PositionalEncoding(tf.keras.layers.Layer):
         # for position [0, 1, 2, ..., max_position_embeddings-1], and the current
         # sequence has positions [0, 1, 2, ... seq_length-1], so we can just
         # perform a slice.
-        self.full_position_embeddings = tf.Variable(
-            initial_value=self.weights_initializer(shape=[config.max_position_embeddings, self.width],dtype='float16'),
-            name="position_embeddings")
-
-    def call(self):
-        full_position_embeddings = self.full_position_embeddings
         position_embeddings = tf.slice(full_position_embeddings, [0, 0],
-                                         [self.seq_length, -1])
+                                         [config.seq_length, -1])
+
         # Only the last two dimensions are relevant (`seq_length` and `width`), so
         # we broadcast among the first dimensions, which is typically just
         # the batch size.
@@ -158,10 +122,46 @@ class PositionalEncoding(tf.keras.layers.Layer):
         num_dims = 3
         for _ in range(num_dims - 2):
             position_broadcast_shape.append(1)
-        position_broadcast_shape.extend([self.seq_length, self.width])
+        position_broadcast_shape.extend([config.seq_length, config.hidden_size])
         position_embeddings = tf.reshape(position_embeddings,
                                          position_broadcast_shape)
         return position_embeddings
+
+
+# class PositionalEncoding(tf.keras.layers.Layer):
+#     def __init__(self, config):
+#         super().__init__()
+#         self.seq_length = config.seq_length
+#         self.width = config.hidden_size
+#         self.weights_initializer = create_initializer(config.initializer_range)
+#         # Since the position embedding table is a learned variable, we create it
+#         # using a (long) sequence length `max_position_embeddings`. The actual
+#         # sequence length might be shorter than this, for faster training of
+#         # tasks that do not have long sequences.
+#         #
+#         # So `full_position_embeddings` is effectively an embedding table
+#         # for position [0, 1, 2, ..., max_position_embeddings-1], and the current
+#         # sequence has positions [0, 1, 2, ... seq_length-1], so we can just
+#         # perform a slice.
+#         self.full_position_embeddings = tf.Variable(
+#             initial_value=self.weights_initializer(shape=[config.max_position_embeddings, self.width],dtype='float16'),
+#             name="position_embeddings")
+
+#     def call(self):
+#         full_position_embeddings = self.full_position_embeddings
+#         position_embeddings = tf.slice(full_position_embeddings, [0, 0],
+#                                          [self.seq_length, -1])
+#         # Only the last two dimensions are relevant (`seq_length` and `width`), so
+#         # we broadcast among the first dimensions, which is typically just
+#         # the batch size.
+#         position_broadcast_shape = []
+#         num_dims = 3
+#         for _ in range(num_dims - 2):
+#             position_broadcast_shape.append(1)
+#         position_broadcast_shape.extend([self.seq_length, self.width])
+#         position_embeddings = tf.reshape(position_embeddings,
+#                                          position_broadcast_shape)
+#         return position_embeddings
         
 
 
@@ -573,8 +573,10 @@ class BertModel(tf.keras.Model):
         token_type_embeddings = tf.reshape(token_type_embeddings,
                                        [batch_size, self.seq_length, self.width])
         x = x + token_type_embeddings
-        x = x + self.pos_encoding()
+        print(f'x before positional encoding layer: {self.pos_encoding}')
+        x = x + self.pos_encoding
         print(f'positional encoding layer: {self.pos_encoding}')
+        print(f'x after positional encoding layer: {self.pos_encoding}')
         x = self.norm_layer(x)
 
         if training:
