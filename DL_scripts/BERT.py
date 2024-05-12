86a1ea6fe3bbd66d709850ed7ed1c0e83ae11f22
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2024-05-11 22:46:14 -0400

    modify position embeddings layer

--- DL_scripts/BERT.py
+++ DL_scripts/BERT.py
@@ -569,7 +569,7 @@ class BertModel(tf.keras.Model):
                                        [batch_size, self.seq_length, self.width])
         x = x + token_type_embeddings
 
-        position_embeddings = self.pos_encoding
+        position_embeddings = self.pos_encoding.get_emb
         # position_embeddings = tf.slice(full_position_embeddings, [0, 0],
         #                                  [config.seq_length, -1])
         # print(f'before position_embeddings: {position_embeddings}')
@@ -588,8 +588,7 @@ class BertModel(tf.keras.Model):
         # print(f'shape of position_embeddings: {tf.shape(position_embeddings)}')
         # print(f'shape of x: {tf.shape(x)}')
         print(f'position_embeddings: {position_embeddings}')
-        print(f'position_embeddings.get_emb: {position_embeddings.get_emb}')
-        x = x + position_embeddings.get_emb
+        x = x + position_embeddings
 
         x = self.norm_layer(x)
 
