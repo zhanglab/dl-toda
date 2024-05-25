1d61ce2d4c90828ae3a7aefa55179d4a23b42567
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2024-05-25 15:47:21 -0400

    correct output from calling model

--- DL_scripts/read_classifier_training.py
+++ DL_scripts/read_classifier_training.py
@@ -177,7 +177,7 @@ def training_step(model_type, data, train_accuracy, loss, opt, model, num_labels
     with tf.GradientTape() as tape:
         if model_type == 'BERT':
             input_ids, input_mask, token_type_ids, labels, is_real_example = data
-            probs, attention_mask = model(input_ids, input_mask, token_type_ids, training)
+            probs = model(input_ids, input_mask, token_type_ids, training)
             # log_probs, probs, logits = model(input_ids, input_mask, token_type_ids, is_training)
             # predictions = tf.argmax(logits, axis=-1, output_type=tf.int32)
             # one_hot_labels = tf.one_hot(labels, depth=num_labels, dtype=tf.float32)
