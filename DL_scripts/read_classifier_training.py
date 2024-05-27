7dcb888d98cafadab542dba7ca53171e389ee736
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2024-05-27 12:06:47 -0400

    add number of labels when calling model

--- DL_scripts/read_classifier_training.py
+++ DL_scripts/read_classifier_training.py
@@ -179,7 +179,7 @@ def training_step(model_type, data, train_accuracy, loss, opt, model, num_labels
         if model_type == 'BERT':
             input_ids, input_mask, token_type_ids, labels, is_real_example = data
             # x, embedding_table, flat_input_ids, input_shape, output_1 = model(input_ids, input_mask, token_type_ids, training)
-            probs = model(input_ids, input_mask, token_type_ids, training)
+            probs = model(num_labels, input_ids, input_mask, token_type_ids, training)
             # log_probs, probs, logits = model(input_ids, input_mask, token_type_ids, is_training)
             # predictions = tf.argmax(logits, axis=-1, output_type=tf.int32)
             # one_hot_labels = tf.one_hot(labels, depth=num_labels, dtype=tf.float32)
@@ -437,7 +437,7 @@ def main():
                     count *= dim
                     total_params += count
                 f.write(f'name = {var.name}, shape = {var.shape}\n')
-                print(f'name = {var.name}, shape = {var.shape}')
+                print(f'name = {var.name}, shape = {var.shape}\t {count}')
             f.write(f'Total params: {total_params}')
 
         # print(model.trainable_weights)
