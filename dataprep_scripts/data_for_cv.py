eeae9efe19774061c723021228a814e7c6d7223b
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2024-06-01 19:40:29 -0400

    correct indentation error

--- dataprep_scripts/data_for_cv.py
+++ dataprep_scripts/data_for_cv.py
@@ -34,9 +34,9 @@ def main():
 		# create output directory for every subset of data
 		fold_dir = os.path.join(args.output_dir, f'cv_subset_{k}')
 		if not os.path.isdir(fold_dir):
-            os.makedirs(fold_dir)
-        val_data = subsets[k]
-        train_data = [subsets[i] for i in range(k) if i != k]
+        	os.makedirs(fold_dir)
+    	val_data = subsets[k]
+    	train_data = [subsets[i] for i in range(k) if i != k]
 		with open(os.path.join(fold_dir, 'train.tsv'), 'w') as out_f:
 			out_f.write(''.join(train_data))
 		with open(os.path.join(fold_dir, 'dev.tsv'), 'w') as out_f:
