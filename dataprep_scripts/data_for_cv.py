69faa47f8e58dfc4d5a5be5b65644aa2abab4c1c
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2024-06-01 22:29:56 -0400

    correct indentation error

--- dataprep_scripts/data_for_cv.py
+++ dataprep_scripts/data_for_cv.py
@@ -50,11 +50,11 @@ def main():
 			for i in range(args.folds):
 				if i != k:
 					train_data = subsets[i]
-					train_data.append(header)
+					train_data.insert(0, header)
 					out_f.write(''.join(subsets[i]))
 		with open(os.path.join(fold_dir, 'dev.tsv'), 'w') as out_f:
 			val_data = subsets[k]
-			val_data.append(header)
+			val_data.insert(0, header)
 			out_f.write(''.join(val_data))
 
 
