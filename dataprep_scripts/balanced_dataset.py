8ee8939952ff6e9d702f1aec59dfdb7c2933d7bf
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2024-07-06 21:58:45 +0200

    add function

--- dataprep_scripts/balanced_dataset.py
+++ dataprep_scripts/balanced_dataset.py
@@ -6,15 +6,7 @@ from collections import defaultdict
 import glob
 
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--input_dir', type=str, help='path to output directory')
-    parser.add_argument('--output_dir', type=str, help='path to output directory', default=os.getcwd())
-    parser.add_argument('--n_reads', type=int, help='number of reads per taxon')
-    parser.add_argument('--label', type=int, help='label associated with taxa')
-    parser.add_argument('--output_filename', type=str, help='name of output fastq file')
-    args = parser.parse_args()
-
+def get_reads(args):
     fq_files = glob.glob(os.path.join(args.input_dir, '*.fq'))
     random.shuffle(fq_files)
     count_reads = 0
@@ -28,12 +20,26 @@ def main():
                     n_line += 1
                     if n_line == 4:
                         label = int(rec.split('\n')[0].rstrip().split('|')[1])
-                        if label != args.label and count_reads <args.n_reads:
-                            out_f.write(rec)
-                            count_reads += 1
+                        if count_reads <args.n_reads:
+                            if label != args.label 
+                                out_f.write(rec)
+                                count_reads += 1
+                        else:
+                            return
                         # initialize variables again
                         n_line = 0
                         rec = ''
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input_dir', type=str, help='path to output directory')
+    parser.add_argument('--output_dir', type=str, help='path to output directory', default=os.getcwd())
+    parser.add_argument('--n_reads', type=int, help='number of reads per taxon')
+    parser.add_argument('--label', type=int, help='label associated with taxa')
+    parser.add_argument('--output_filename', type=str, help='name of output fastq file')
+    args = parser.parse_args()
+
+    get_reads(args)
 
 
 if __name__ == "__main__":
