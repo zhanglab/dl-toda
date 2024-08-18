8bdf807a624e1681b7e57059b46c32e12225b2ac
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2024-08-18 08:10:48 -0400

    create vocab with multiprocessing lib

--- dataprep_scripts/vocab.py
+++ dataprep_scripts/vocab.py
@@ -98,7 +98,7 @@ if __name__ == "__main__":
             list_can_kmers += can_kmers[str(process)]
         print(len(list_can_kmers))
         # get unique canonical kmers
-        list_can_kmers = list(set(list_can_kmers)).sort()
+        list_can_kmers = sorted(list(set(list_can_kmers)))
         print(len(list_can_kmers))
         with open(f'{k_value}mers.txt', 'w') as f:
             if model == 'BERT':
