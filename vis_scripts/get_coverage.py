08b4b1038a66ed8e6fa68fcaa8f24c59588aa57d
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2024-08-21 14:56:20 -0400

    use multiprocessing library

--- vis_scripts/get_coverage.py
+++ vis_scripts/get_coverage.py
@@ -23,7 +23,7 @@ def get_coverage(list_of_reads, length_ref, results, process_id):
     # for each reference in the dictionary, create a new dictionary with the
     # number of matches at each position encountered
     # dict_coverage = defaultdict(lambda : 0)
-    dict_coverage = {i: 0 for i in range(1, length_ref, 1)}
+    dict_coverage = {i: 0 for i in range(length_ref)}
 
     for j in range(0, len(list_of_reads)):
         read_start = list_of_reads[j][0] - 1 # read_start = list_of_reads[j][0] - 1
