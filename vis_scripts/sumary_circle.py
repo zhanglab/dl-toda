c168e452740c6a5eb5bdf49098f53f99853e2949
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2024-08-25 13:35:17 -0400

    print line instead of bar chart

--- vis_scripts/sumary_circle.py
+++ vis_scripts/sumary_circle.py
@@ -87,7 +87,7 @@ def main():
 	    line_track.axis()
 	    line_track.xticks_by_interval(5000, label_formatter=lambda v: f"{ len(pos_coverage) / 1000:.0f} Kb")
 	    line_track.xticks_by_interval(1000, tick_length=1, show_label=False)
-	    line.track.line(base_positions, pos_coverage)
+	    line_track.line(base_positions, pos_coverage)
 	    # bar_track.bar(base_positions, pos_coverage)
 	circos.savefig(os.path.join(output_dir, f'sum_circos.png'))
 
