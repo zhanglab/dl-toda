34bf93a2d3332b7ef4204fdd630d5b376a1a3b42
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2024-08-26 20:14:49 -0400

    add multiprocessing lib

--- vis_scripts/summary_circle.py
+++ vis_scripts/summary_circle.py
@@ -71,7 +71,7 @@ def plot_circles(base_positions, pos_coverage, pos_conf_scores, neg_conf_scores,
 	# define x axis
 	# base_positions = list(range(0,len(pos_coverage),1))
 	# initialize a single circos sector
-	sectors = {'genome': len(pos_coverage)}
+	sectors = {'genome': len(base_positions)}
 	circos = Circos(sectors=sectors, space=10)
 
 	# define min and max y values
