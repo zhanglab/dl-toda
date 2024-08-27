eee125a5c62e15297355e44608c3e2ae468e11c3
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2024-08-27 14:07:52 -0400

    add tracks for coverage of training reads

--- vis_scripts/summary_circle.py
+++ vis_scripts/summary_circle.py
@@ -22,10 +22,11 @@ def prep_test_results(testing_output, alignment_sum, reads_id, label, ref_length
 
 	print(f'# test reads: {len(test_results)}\n # test reads mapped to training genome: {len(map_info)}')
 
-	pos_label = [0]*ref_length # number of reads given the positive label at every position in the genome
-	neg_label = [0]*ref_length
-	pos_conf_scores = [0.0]*ref_length
-	neg_conf_scores = [0.0]*ref_length
+	pos_label = defaultdict(int)
+	neg_label = defaultdict(int)
+	pos_conf_scores = defaultdict(float)
+	neg_conf_scores = defaultdict(float)
+
 	for r in reads_id:
 		if r.split('|')[1] == label:
 			# check if read mapped to the genome
@@ -45,40 +46,45 @@ def prep_test_results(testing_output, alignment_sum, reads_id, label, ref_length
 						neg_label[i] += 1
 						neg_conf_scores[i] += cs
 
+	if sort(list(pos_conf_scores.keys())) == sort(list(pos_label.keys())):
+		print('both dicts have the same keys')
+
+	if sort(list(neg_conf_scores.keys())) == sort(list(neg_label.keys())):
+		print('both dicts have the same keys')
+
+
+
 	# get average of cs
-	for i in range(ref_length):
-		if pos_label[i] != 0:
-			pos_conf_scores[i] = round(pos_conf_scores[i]/pos_label[i], 2)
-		if neg_label[i] != 0:
-			neg_conf_scores[i] = round(neg_conf_scores[i]/neg_label[i], 2)
+	for k, v in pos_label.items():
+		pos_conf_scores[k] = pos_conf_scores[k]/v
+		pos_label_percent[k] = v
+		print(type(k))
+
+	for k, v in neg_label.items():
+		neg_conf_scores[k] = neg_conf_scores[k]/v
 
 	# get percentages of positive and negative labels
-	pos_label_percent = []
-	neg_label_percent = []
+	pos_label_percent = {}
+	neg_label_percent = {}
 	for i in range(ref_length):
-		if pos_label[i] + neg_label[i] != 0:
-			pos_label_percent.append(pos_label[i]/(pos_label[i]+neg_label[i]))
-			neg_label_percent.append(neg_label[i]/(pos_label[i]+neg_label[i]))
-		else:
-			pos_label_percent.append(0)
-			neg_label_percent.append(0)
+		if i in neg_label and i in pos_label:
+			pos_label_percent[i] = pos_label[i]/(pos_label[i]+neg_label[i])
+			neg_label_percent[i] = neg_label[i]/(pos_label[i]+neg_label[i])
+		if i in neg_label and i not in pos_label:
+			neg_label_percent[i] = 1.0
+		if i not in neg_label and i in pos_label:
+			pos_label_percent[i] = 1.0
 
 	return pos_label_percent, neg_label_percent, pos_conf_scores, neg_conf_scores
 
 
-def plot_circles(output_dir, base_positions, pos_coverage, pos_conf_scores, neg_conf_scores, pos_label, neg_label, number):
-	# define x axis
-	# base_positions = list(range(0,len(pos_coverage),1))
+def plot_circles(output_dir, base_positions, test_pos_coverage, train_pos_coverage, pos_conf_scores, neg_conf_scores, pos_label, neg_label):
+	# def plot_circles(output_dir, base_positions, pos_coverage, pos_conf_scores, neg_conf_scores, pos_label, neg_label, number):
+
 	# initialize a single circos sector
 	sectors = {'genome': len(base_positions)}
 	circos = Circos(sectors=sectors, space=10)
-	print(len(list(range(0,len(base_positions),1))))
-	# define min and max y values
-	cs_min = min(pos_conf_scores+neg_conf_scores)
-	cs_max = max(pos_conf_scores+neg_conf_scores)
-	labels_min = min(pos_label+neg_label)
-	labels_max = max(pos_label+neg_label)
-	print(f'cs_min: {cs_min}\tcs_max: {cs_max}\tlabels_min: {labels_min}\tlabels_max: {labels_max}')
+
 	for sector in circos.sectors:
 		# add outer track
 		genome_track = sector.add_track((98, 100))
@@ -87,66 +93,88 @@ def plot_circles(output_dir, base_positions, pos_coverage, pos_conf_scores, neg_
 		base_pos_ticks = [base_positions[i] for i in genome_x]
 		genome_x_labels = [str(i) for i in base_pos_ticks]
 		genome_track.xticks(genome_x, genome_x_labels)
-		# genome_track.xticks_by_interval(100000, label_formatter=lambda v: f"{v / 1000:.0f} Kb")
 		genome_track.xticks_by_interval(1000, tick_length=1, show_label=False)
 		print(f'added genome track')
-		# add track for coverage
-		print(len(base_positions), len(pos_coverage))
-		cov_track = sector.add_track((85, 95))
+		# add track for coverage of training reads
+		print(len(base_positions), len(train_pos_coverage))
+		cov_track = sector.add_track((87, 97))
 		cov_track.axis()
-		cov_y = list(range(min([int(i) for i in pos_coverage]), max([math.ceil(j) for j in pos_coverage])+1, 2))
+		cov_y = list(range(min([int(i) for i in train_pos_coverage]), max([math.ceil(j) for j in train_pos_coverage])+1, 2))
 		cov_y_labels = list(map(str, cov_y))
 		cov_track.yticks(cov_y, cov_y_labels)
-		cov_track.line(list(range(0,len(base_positions),1)), pos_coverage, color="0")
+		cov_track.line(list(range(0,len(base_positions),1)), train_pos_coverage, color="#CFF800")
+		print(f'added coverage track')
+		# add track for coverage of testing reads
+		print(len(base_positions), len(test_pos_coverage))
+		cov_track = sector.add_track((76, 86))
+		cov_track.axis()
+		cov_y = list(range(min([int(i) for i in test_pos_coverage]), max([math.ceil(j) for j in test_pos_coverage])+1, 2))
+		cov_y_labels = list(map(str, cov_y))
+		cov_track.yticks(cov_y, cov_y_labels)
+		cov_track.line(list(range(0,len(base_positions),1)), test_pos_coverage, color="#CO5780")
 		print(f'added coverage track')
 		# add track for labels predicted as positive
-		pos_labels_track = sector.add_track((72, 82))
+		pos_labels_track = sector.add_track((65, 75))
 		pos_labels_track.axis()
 		pos_labels_y = [0.0, 0.5, 1.0]
 		pos_labels_y_labels = list(map(str, pos_labels_y))
 		pos_labels_track.yticks(pos_labels_y, pos_labels_y_labels)
-		pos_labels_track.bar(list(range(0,len(base_positions),1)), pos_label, color="#BF2C34")
-		# pos_labels_track.heatmap(pos_label, vmin=labels_min, vmax=labels_max, show_value=False)
+		pos_labels_x = sort(list(pos_label.keys))
+		pos_labels_x_values = [pos_label[i] for i in pos_labels_x]
+		pos_labels_track.line(pos_labels_x, pos_labels_x_values, color="#FF828B")
 		print(f'added pos labels track')
 		# add track for the confidence scores assigned to labels predicted as positive
-		pos_cs_track = sector.add_track((59, 69))
+		pos_cs_track = sector.add_track((54, 64))
 		pos_cs_track.axis()
 		pos_cs_y = [0.0, 0.5, 1.0]
 		pos_cs_y_labels = list(map(str, pos_cs_y))
 		pos_cs_track.yticks(pos_cs_y, pos_cs_y_labels)
-		pos_cs_track.bar(list(range(0,len(base_positions),1)), pos_conf_scores, color="#5C62D6")
-		# pos_cs_track.heatmap(pos_conf_scores, vmin=cs_min, vmax=cs_max, show_value=False)
+		pos_cs_x = sort(list(pos_conf_scores.keys))
+		pos_cs_x_values = [pos_conf_scores[i] for i in pos_cs_x]
+		pos_cs_track.scatter(pos_cs_x, pos_cs_x_values, color="#E7C582")
 		print(f'added pos cs track')
 		# add track for labels predicted as negative
-		neg_labels_track = sector.add_track((46, 56))
+		neg_labels_track = sector.add_track((43, 53))
 		neg_labels_track.axis()
 		neg_labels_y = [0.0, 0.5, 1.0]
 		neg_labels_y_labels = list(map(str, neg_labels_y))
 		neg_labels_track.yticks(neg_labels_y, neg_labels_y_labels)
-		neg_labels_track.bar(list(range(0,len(base_positions),1)), neg_label, color="#BE398D")
+		neg_labels_x = sort(list(neg_label.keys))
+		neg_labels_x_values = [neg_label[i] for i in neg_labels_x]
+		neg_labels_track.line(neg_labels_x, neg_labels_x_values, color="#00BOBA")
 		print(f'added neg labels track')
 		# add track for the confidence scores assigned to labels predicted as negative
-		neg_cs_track = sector.add_track((33, 43))
+		neg_cs_track = sector.add_track((32, 42))
 		neg_cs_track.axis()
 		neg_cs_y = [0.0, 0.5, 1.0]
 		neg_cs_y_labels = list(map(str, neg_cs_y))
 		neg_cs_track.yticks(neg_cs_y, neg_cs_y_labels)
-		neg_cs_track.bar(list(range(0,len(base_positions),1)), neg_conf_scores, color="#F07857")
+		neg_cs_x = sort(list(neg_conf_scores.keys))
+		neg_cs_x_values = [neg_conf_scores[i] for i in neg_cs_x]
+		neg_cs_track.bar(neg_cs_x, neg_cs_x_values, color="#0065A2")
 		print(f'added neg cs track')
-	
-	# circos.colorbar(bounds=(0.35, 0.55, 0.3, 0.01), vmin=labels_min, vmax=labels_max, orientation="horizontal", cmap="viridis")
-	# circos.colorbar(bounds=(0.35, 0.45, 0.3, 0.01), vmin=cs_min, vmax=cs_max, orientation="horizontal", cmap="plasma")
 
-	circos.savefig(os.path.join(output_dir, f'sum_circos_{number}.png'))
+	# circos.savefig(os.path.join(output_dir, f'sum_circos_{number}.png'))
+	circos.savefig(os.path.join(output_dir, f'sum_circos.png'))
+
+
+def get_coverage(filename):
+	with open(filename, 'r') as f:
+		content = f.readlines()
+		pos_coverage = [math.log(int(i.rstrip().split('\t')[1])) if int(i.rstrip().split('\t')[1]) != 0 else 0.0 for i in content]
+		base_positions = list(range(0,len(pos_coverage),1))
+
+	return pos_coverage, base_positions
 
 
 def main():
 	fq_file = sys.argv[1]
 	testing_output = sys.argv[2]
-	genome_cov = sys.argv[3]
-	alignment_sum = sys.argv[4]
-	label = sys.argv[5]
-	output_dir = sys.argv[6]
+	train_reads_genome_cov = sys.argv[3]
+	test_reads_genome_cov = sys.argv[4]
+	alignment_sum = sys.argv[5]
+	label = sys.argv[6]
+	output_dir = sys.argv[7]
 
 	# get reads id
 	num_lines = 4
@@ -154,13 +182,12 @@ def main():
 	reads_id = [r.split("\n")[0][1:] for r in reads]
 	print(f'# reads: {len(reads_id)}\t{len(reads)}\t{reads_id[0]}')
 
-	# get coverage of target genome per position
-	with open(genome_cov, 'r') as f:
-		content = f.readlines()
-		pos_coverage = [math.log(int(i.rstrip().split('\t')[1])) if int(i.rstrip().split('\t')[1]) != 0 else 0.0 for i in content]
-		base_positions = list(range(0,len(pos_coverage),1))
-
-	pos_label, neg_label, pos_conf_scores, neg_conf_scores = prep_test_results(testing_output, alignment_sum, reads_id, label, len(pos_coverage))
+	# load coverage of training reads to training genome
+	train_pos_coverage, base_positions = get_coverage(train_reads_genome_cov)
+	# load coverage of testing reads to training genome
+	test_pos_coverage, _ = get_coverage(test_reads_genome_cov) 
+	# load testing results
+	pos_label, neg_label, pos_conf_scores, neg_conf_scores = prep_test_results(testing_output, alignment_sum, reads_id, label, len(base_positions))
 
 	print(f'{len(pos_coverage)}\t{len(pos_label)}\t{len(neg_label)}\t{len(pos_conf_scores)}\t{len(neg_conf_scores)}')
 	print(f'{pos_label[:10]}\n{pos_conf_scores[:10]}\n{neg_label[:10]}\n{neg_conf_scores[:10]}\n{pos_coverage[:10]}')
@@ -169,31 +196,24 @@ def main():
 	print(f'labels_min: {min(neg_label)}\tlabels_max: {max(neg_label)}')
 	print(f'cov_min: {min(pos_coverage)}\tcov_max: {max(pos_coverage)}')
 
-	# divide data into 5 subsets and create a circle plot for each subset
-	num_subsets = 20
-	subset_size = math.ceil(len(pos_coverage)/num_subsets)
-	pos_label_subsets = [pos_label[i:i+subset_size] for i in range(0, len(pos_label), subset_size)]
-	neg_label_subsets = [neg_label[i:i+subset_size] for i in range(0, len(neg_label), subset_size)]
-	pos_cs_subsets = [pos_conf_scores[i:i+subset_size] for i in range(0, len(pos_conf_scores), subset_size)]
-	neg_cs_subsets = [neg_conf_scores[i:i+subset_size] for i in range(0, len(neg_conf_scores), subset_size)]
-	pos_cov_subsets = [pos_coverage[i:i+subset_size] for i in range(0, len(pos_coverage), subset_size)]
-	base_pos_subsets = [base_positions[i:i+subset_size] for i in range(0, len(base_positions), subset_size)]
-	print(f'{pos_label_subsets[0][:10]}')
-	print(f'{neg_label_subsets[0][:10]}')
-	with mp.Manager() as manager:
-		processes = [mp.Process(target=plot_circles, args=(output_dir, base_pos_subsets[i], pos_cov_subsets[i], pos_cs_subsets[i], neg_cs_subsets[i], pos_label_subsets[i], neg_label_subsets[i], i)) for i in range(num_subsets)]
-		for p in processes:
-			p.start()
-		for p in processes:
-			p.join()
+	# # divide data into 5 subsets and create a circle plot for each subset
+	# num_subsets = 20
+	# subset_size = math.ceil(len(pos_coverage)/num_subsets)
+	# pos_label_subsets = [pos_label[i:i+subset_size] for i in range(0, len(pos_label), subset_size)]
+	# neg_label_subsets = [neg_label[i:i+subset_size] for i in range(0, len(neg_label), subset_size)]
+	# pos_cs_subsets = [pos_conf_scores[i:i+subset_size] for i in range(0, len(pos_conf_scores), subset_size)]
+	# neg_cs_subsets = [neg_conf_scores[i:i+subset_size] for i in range(0, len(neg_conf_scores), subset_size)]
+	# pos_cov_subsets = [pos_coverage[i:i+subset_size] for i in range(0, len(pos_coverage), subset_size)]
+	# base_pos_subsets = [base_positions[i:i+subset_size] for i in range(0, len(base_positions), subset_size)]
+	# with mp.Manager() as manager:
+	# 	processes = [mp.Process(target=plot_circles, args=(output_dir, base_pos_subsets[i], pos_cov_subsets[i], pos_cs_subsets[i], neg_cs_subsets[i], pos_label_subsets[i], neg_label_subsets[i], i)) for i in range(num_subsets)]
+	# 	for p in processes:
+	# 		p.start()
+	# 	for p in processes:
+	# 		p.join()
 
-	# total = 0
-	# for i in range(6):
-	# 	print(base_pos_subsets[i][0], base_pos_subsets[i][-1])
-	# 	print(len(base_pos_subsets[i]), len(pos_cov_subsets[i]))
-	# 	total += len(base_pos_subsets[i])
-	# 	plot_circles(base_pos_subsets[i], pos_cov_subsets[i], pos_cs_subsets[i], neg_cs_subsets[i], pos_label_subsets[i], pos_label_subsets[i], i)
-	# print(total)
+	plot_circles(output_dir, base_positions, test_pos_coverage, train_pos_coverage, pos_conf_scores, neg_conf_scores, pos_label, neg_label)
+
 	
 
 
