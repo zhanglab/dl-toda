e94f5edf4a63c96702d3f844ca1d364bd5dbbfa3
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2024-09-03 11:07:25 -0400

    update creation of output file

--- vis_scripts/get_testing_epoch.py
+++ vis_scripts/get_testing_epoch.py
@@ -7,7 +7,7 @@ def main():
 	val_data = sys.argv[1]
 	output_dir = sys.argv[2]
 
-	with open(os.path.join(val_data, 'validation_data_rnd_1.tsv'), 'r') as f:
+	with open(val_data, 'r') as f:
 		content = f.readlines()
 		loss = [float(i.rstrip().split('\t')[2]) for i in content]
 
@@ -19,7 +19,7 @@ def main():
 			epoch_min_value = i
 	
 	print(min_value, epoch_min_value)
-	with open(os.path.join(output_dir, 'testing_epoch.tsv'), 'a') as f:
+	with open(os.path.join(output_dir, 'testing_epoch.tsv'), 'w') as f:
 		f.write(f'{epoch_min_value}\t{min_value}\n')
 
 
