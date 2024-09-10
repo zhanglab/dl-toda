3e1edd5bd3663cc1fca21a044f4d71768ab96f16
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2024-09-10 19:15:35 -0400

    update parsing of features in tfrec

--- DL_scripts/read_classifier_training.py
+++ DL_scripts/read_classifier_training.py
@@ -174,9 +174,9 @@ def build_dataset(filenames, batch_size, vector_size, num_classes, datatype, is_
           "input_ids": tf.io.FixedLenFeature([vector_size], tf.int64),
           "input_mask": tf.io.FixedLenFeature([vector_size], tf.int64),
           "segment_ids": tf.io.FixedLenFeature([vector_size], tf.int64),
-          "masked_lm_positions": tf.io.FixedLenFeature([], tf.int64),
-          "masked_lm_weights": tf.io.FixedLenFeature([], tf.float32),
-          "masked_lm_ids": tf.io.FixedLenFeature([], tf.int64),
+          "masked_lm_positions": tf.io.FixedLenFeature([37], tf.int64),
+          "masked_lm_weights": tf.io.FixedLenFeature([37], tf.float32),
+          "masked_lm_ids": tf.io.FixedLenFeature([37], tf.int64),
           "next_sentence_labels": tf.io.FixedLenFeature([], tf.int64),
         }
         # load one example
