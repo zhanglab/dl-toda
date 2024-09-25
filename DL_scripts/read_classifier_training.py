e09ab7ec58e897665d9d0828fc2df459750dbd10
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2024-09-25 15:25:31 -0400

    update bert config

--- DL_scripts/read_classifier_training.py
+++ DL_scripts/read_classifier_training.py
@@ -660,11 +660,12 @@ def main():
 
     start = datetime.datetime.now()
     print('start training')
+    logits = model(**train_inputs).logits
+    print(logits)
+    
     for batch, data in enumerate(train_input.take(num_train_steps), 1):
-        print(f'batch: {batch}\tdata: {data}')
         if args.bert_huggingface:
             logits = model(**data).logits
-            print(logits)
         else:
             loss_value, loss_value_1 = training_step(args.model_type, args.bert_step, data, num_labels, train_accuracy, loss, opt, model, batch == 1)
             # if batch % 100 == 0 and hvd.rank() == 0:
