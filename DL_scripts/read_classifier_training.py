0f219e4e8a7b34df84a7ec1b990c3dea97af1233
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2024-09-25 11:40:39 -0400

    update bert config call

--- DL_scripts/read_classifier_training.py
+++ DL_scripts/read_classifier_training.py
@@ -628,10 +628,7 @@ def main():
         with open(args.bert_config_file, "r") as f:
             args.config_dict = json.load(f)
         # create BERT config obkect
-        bert_config = BertConfig("attention_probs_dropout_prob": 0.1, "classifier_dropout": null, "hidden_act": "gelu", "hidden_dropout_prob": 0.1, "hidden_size": 768, "initializer_range": 0.02, \
-                "intermediate_size": 3072, "layer_norm_eps": 1e-12, "max_position_embeddings": 512, "model_type": "bert", \
-                "num_attention_heads": 12, "num_hidden_layers": 12, "pad_token_id": 0, "position_embedding_type": "absolute", \
-                "transformers_version": "4.44.2", "type_vocab_size": 2, "use_cache": true, "vocab_size": args.config_dict["vocab_size"])
+        bert_config = BertConfig("vocab_size": args.config_dict["vocab_size"])
 
         model = TFBertForSequenceClassification(bert_config)
     else:
