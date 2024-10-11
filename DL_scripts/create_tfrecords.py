d2fe272b90304f18acda4cb06cda413c2a925aeb
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2024-10-11 08:13:20 -0400

    update loading tfrecords

--- DL_scripts/create_tfrecords.py
+++ DL_scripts/create_tfrecords.py
@@ -271,7 +271,7 @@ def create_tfrecords(args, data):
                     if len(dna_list) < args.kmer_vector_length:
                         num_padded_values = args.kmer_vector_length-len(dna_list)
                         dna_list = dna_list + [args.dict_kmers['[PAD]']] * num_padded_values
-                    if len(dna_list) == 508:
+                    if len(dna_list) > args.kmer_vector_length:
                         print(f'{len(dna_list)}\t{dna_list}\t{args.kmer_vector_length}\t{args.max_read_length}\n')
                 else:
                     dna_list  = prepare_input_data(args, dna_sequences[i])      
