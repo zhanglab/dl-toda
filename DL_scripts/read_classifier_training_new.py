20573b770b5031ab297fb4c43b7cb932ed2aedb7
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2024-10-25 11:46:06 -0400

    add script

--- DL_scripts/read_classifier_training_new.py
+++ DL_scripts/read_classifier_training_new.py
@@ -819,10 +819,12 @@ def main():
     for batch, data in enumerate(train_input.take(num_train_steps), 1):        
         mask_token_index = training_step(args.model_type, args.bert_step, data, num_labels, train_accuracy, loss, opt, model, batch == 1)
         print(f'input_ids: {data["input_ids"][0]}')
-        print(f'mask_token_index: {len(mask_token_index)}')
+        print(f'mask_token_index: {mask_token_index}\t{len(mask_token_index)}')
         print(f'labels: {data["labels"][0]}')
-        mask_token_values = tf.where(data["labels"][0] != -100)
-        print(f'mask_token_values: {mask_token_values}\t{len(mask_token_values)}')
+        mask_token_values_1 = tf.where(data["labels"][0] != -100)
+        mask_token_values_1 = tf.where(data["input_ids"][0] == 4)
+        print(f'mask_token_values_1: {mask_token_values_1}\t{len(mask_token_values_1)}')
+        print(f'mask_token_values_2: {mask_token_values_2}\t{len(mask_token_values_2)}')
         break
     #     # if batch == 1:
     #     #     all_labels = [labels]
