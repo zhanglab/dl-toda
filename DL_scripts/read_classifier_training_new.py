029d51aa7698613961ebcdec5df039e60eb1f40f
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2024-10-26 10:18:22 -0400

    compute train loss for bert pretraining

--- DL_scripts/read_classifier_training_new.py
+++ DL_scripts/read_classifier_training_new.py
@@ -396,7 +396,7 @@ def training_step(model_type, bert_step, data, num_labels, train_accuracy, loss,
             # outputs = model(**data).logits
             outputs = model(input_ids=data["input_ids"], token_type_ids=data["token_type_ids"], attention_mask=data["attention_mask"], labels=data["labels"])
             # shape of logits: (batch_size, max_embedding_size==512, vocab_size)
-            logits = outputs.logits. 
+            logits = outputs.logits
             loss_value = outputs.loss
             # retrieve index of masked tokens (tokens that have been replaced by 'MASK')
             mask_token_index_1 = tf.where((data["input_ids"] == 4)[0])
