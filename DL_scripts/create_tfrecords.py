deabb14f0d29a023a70f19d30eff763f14ab2a77
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2024-10-26 07:43:51 -0400

    add print statement

--- DL_scripts/create_tfrecords.py
+++ DL_scripts/create_tfrecords.py
@@ -147,9 +147,9 @@ def process_dnabert_data(args, dna_sequences, labels):
     max_position_embeddings = 512 # define the maximum sequence length the model can encounter in the dataset
     data = []
     for i in range(len(dna_sequences)):
-        # seq = dna_sequences[i][0]
-        # for k in dna_sequences[i][1:]:
-        #     seq += k[-1]
+        seq = dna_sequences[i][0]
+        for k in dna_sequences[i][1:]:
+            seq += k[-1]
     
         # parse dna sequence
         dna_list = [args.dict_kmers[kmer] if kmer in args.dict_kmers else args.dict_kmers['[UNK]'] for kmer in dna_sequences[i]]
