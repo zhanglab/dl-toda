f3a1b0132b2b360fa9c5613081c18bc00d98794d
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2024-11-09 08:42:39 -0500

    get tokens from tokens id

--- DL_scripts/vis_attentions.py
+++ DL_scripts/vis_attentions.py
@@ -162,10 +162,9 @@ def main():
     test_input = build_dataset(args, test_file, num_labels, is_training=False, drop_remainder=False)
 
     for batch, data in enumerate(test_input.take(test_steps), 1):
-        outputs = model(**data)
-        attentions = outputs[-1]
-        # attentions = get_attentions(data, model)
-        print(f'attentions : {attentions}') 
+        list(attentions) = get_attentions(data, model)
+        print(f'attentions : {len(attentions)}') 
+        print(attentions[-1].shape)
 
         # get kmers of ids
         print(f'input ids: {data["input_ids"]}')
