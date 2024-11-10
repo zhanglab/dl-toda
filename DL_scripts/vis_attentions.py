982ef5771e1da33d40009c5180bf53e2cb06054a
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2024-11-10 14:22:25 -0500

    get attentions

--- DL_scripts/vis_attentions.py
+++ DL_scripts/vis_attentions.py
@@ -209,7 +209,7 @@ def main():
             print(df.shape)
             print(df)
             plt.figure(figsize=(10, 10))
-            sn.heatmap(data=attentions_weights, annot=True, xticklabels=df.columns, yticklabels=df.columns) 
+            sn.heatmap(data=df, annot=True, xticklabels=df.columns, yticklabels=df.columns) 
             plt.savefig(os.path.join(args.output_dir, 'attention_weights_heatmap.png'))
             # get list of relevant kmers
 
