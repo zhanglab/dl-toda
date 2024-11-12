64ee0321a70f002840785a31a1979b2257f3286a
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2024-11-11 20:12:52 -0500

    concatenate attentions from all attention heads

--- DL_scripts/vis_attentions.py
+++ DL_scripts/vis_attentions.py
@@ -212,32 +212,17 @@ def main():
             df = df.drop(pad_idx, axis='index')
             # remove columns ['PAD']
             df = df.drop('[PAD]', axis='columns')
+            # get kmers with high attention weights
+            filtered_df = df.loc[:, (df >= np.mean(df.values.tolist())).any()]
+
             if label == 0:
                 attention_weights_label_0.append(df.values.flatten().tolist())
                 df_label_0.append(df)
+                kmers_label_0 += filtered_df.columns.tolist()
             else:
                 attention_weights_label_1.append(df.values.flatten().tolist())
                 df_label_1.append(df)
-
-            # get kmers with high attention weights
-            filtered_df = df.loc[:, (df >= np.mean(df.values.tolist())).any()]
-            print(filtered_df)
-            print(filtered_df.columns.tolist())
-            print(len(filtered_df.columns.tolist()))
-
-            # get stats on attention weights
-            print(f'Stats on attentions:\nMean: {np.mean(df.values.tolist())}\tSd: {np.std(df.values.tolist())}\t'
-                f'Median: {np.median(df.values.tolist())}\tMin: {np.min(df.values.tolist())}\tMax: {np.max(df.values.tolist())}\t'
-                f'Sum: {np.sum(df.values.tolist())}')
-
-        #     palette = sn.color_palette("icefire", as_cmap=True)
-        #     # for i in range(len(df_label_0)):
-        #     plt.figure(figsize=(15, 15))
-        #     sn.heatmap(data=df, annot=False, xticklabels=df.columns, yticklabels=df.columns, cmap=palette) 
-        #     plt.savefig(os.path.join(args.output_dir, f'attention_weights_heatmap_{i}_{len(df)}_label.png'))
-
-            break
-        break
+                kmers_label_1 += filtered_df.columns.tolist()
     
     # set color palette
     palette = sn.color_palette("icefire", as_cmap=True)
@@ -267,6 +252,16 @@ def main():
     plt.ylabel('Frequency')
     plt.grid(True)
     plt.savefig(os.path.join(args.output_dir, 'attention_weights_hist_label.png'))
+
+    # store list of relevant kmers
+    print(f'# relevant kmers for label 0: {len(set(kmers_label_0))}')
+    print(f'# relevant kmers for label 1: {len(set(kmers_label_1))}')
+
+    with open(os.path.join(args.output_dir, 'relevant_kmers_other'), 'w') as f:
+        f.write('\n'.join(kmers_label_0))
+
+    with open(os.path.join(args.output_dir, 'relevant_kmers_label'), 'w') as f:
+        f.write('\n'.join(kmers_label_1))
 
 
         # 1. get species with high performance
