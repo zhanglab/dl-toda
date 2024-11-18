2c8557eb6727e40b129c13ca9915f04a9327c1c4
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2024-11-18 18:12:43 -0500

    correct call to function

--- vis_scripts/summary_circle.py
+++ vis_scripts/summary_circle.py
@@ -357,7 +357,7 @@ def main():
 	reads_id = UnmappedReads(args, mapping_info)
 
     # get mean confidence scores at each position of the testing genome
-	confidence_scores = GetConfidenceScores(args testing_genome_length, mapping_info, reads_id)
+	confidence_scores = GetConfidenceScores(args, testing_genome_length, mapping_info, reads_id)
 
     # get coverage of training genome
 	pos_coverage, genome_positions = GetCoverage(args.test_coverage)
