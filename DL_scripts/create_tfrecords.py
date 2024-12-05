064d1709d06c3fc3a0935cbc7c7c1833f4fd32cb
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2024-12-04 23:03:36 -0500

    provide full path to ckpt path

--- DL_scripts/create_tfrecords.py
+++ DL_scripts/create_tfrecords.py
@@ -219,12 +219,13 @@ def process_dnabert_data(args, dna_sequences, labels):
         if args.bert_step == 'pretraining':
             # data.append([dna_list, attention_mask, token_type_ids, labels, next_sentence_label])
             data.append([dna_list, attention_mask, position_ids, labels])       
-            with open(args.info, 'w') as f:
-                f.write(f'{min(n_masked_pos)}\t{max(n_masked_pos)}\t{statistics.mean(n_masked_pos)}\t{statistics.median(n_masked_pos)}')
         else:
             # data.append([dna_list, attention_mask, token_type_ids, labels[i]])
             data.append([dna_list, attention_mask, position_ids, labels[i]])
 
+    with open(args.info, 'w') as f:
+        f.write(f'{min(n_masked_pos)}\t{max(n_masked_pos)}\t{statistics.mean(n_masked_pos)}\t{statistics.median(n_masked_pos)}')
+        
     return data
 
 
