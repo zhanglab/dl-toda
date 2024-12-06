59638ea414b1de3fc016524582ec272afba4c866
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2024-12-06 10:40:39 -0500

    update processing dna sequences for finetuning

--- DL_scripts/create_tfrecords.py
+++ DL_scripts/create_tfrecords.py
@@ -223,8 +223,9 @@ def process_dnabert_data(args, dna_sequences, labels):
             # data.append([dna_list, attention_mask, token_type_ids, labels[i]])
             data.append([dna_list, attention_mask, position_ids, labels[i]])
 
-    with open(args.info, 'w') as f:
-        f.write(f'{min(n_masked_pos)}\t{max(n_masked_pos)}\t{statistics.mean(n_masked_pos)}\t{statistics.median(n_masked_pos)}')
+    if args.bert_step == 'pretraining':
+        with open(args.info, 'w') as f:
+            f.write(f'{min(n_masked_pos)}\t{max(n_masked_pos)}\t{statistics.mean(n_masked_pos)}\t{statistics.median(n_masked_pos)}')
         
     return data
 
