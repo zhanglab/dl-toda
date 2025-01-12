8c650e917bc16814fd059b6652d58dc1f450f847
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2025-01-12 12:58:15 -0500

    add from_pretrained function

--- DL_scripts/read_classifier.py
+++ DL_scripts/read_classifier.py
@@ -419,16 +419,18 @@ def main():
             args.config_dict = json.load(f)
         # update input vector size
         args.vector_size = args.config_dict['max_position_embeddings']
-
-    if args.model is not None:
-        model = tf.keras.models.load_model(args.model)
-    elif args.ckpt is not None:
-        # load model with checkpoint
-        if args.model_type == 'BERT_HUGGINGFACE':
-            # create BERT config object + model
-            bert_config = BertConfig(vocab_size=args.config_dict["vocab_size"])
-            model = TFBertForSequenceClassification(config=bert_config)            
-        else:
+        bert_config = BertConfig(vocab_size=args.config_dict["vocab_size"])
+        model = TFBertForSequenceClassification.from_pretrained(args.pretrained, config=bert_config)
+    else:
+        if args.model is not None:
+            model = tf.keras.models.load_model(args.model)
+        elif args.ckpt is not None:
+            # load model with checkpoint
+            # if args.model_type == 'BERT_HUGGINGFACE':
+            #     # create BERT config object + model
+            #     bert_config = BertConfig(vocab_size=args.config_dict["vocab_size"])
+            #     model = TFBertForSequenceClassification(config=bert_config)            
+            # else:
             model = models[args.model_type](args, args.vector_size, args.embedding_size, num_labels, vocab_size, args.dropout_rate)
                
         # the following 2 restore() functions and load_weights function raise warnings about inconsistent references, the model is not correctly loaded
