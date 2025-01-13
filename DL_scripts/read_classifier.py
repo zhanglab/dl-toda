18b0a6676a2e4d09b120ba273d37dfd50e10bfe7
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2025-01-13 13:50:08 -0500

    use from_pretrained to test bert

--- DL_scripts/read_classifier.py
+++ DL_scripts/read_classifier.py
@@ -421,25 +421,25 @@ def main():
         # update input vector size
         args.vector_size = args.config_dict['max_position_embeddings']
         bert_config = BertConfig(vocab_size=args.config_dict["vocab_size"])
-    #     model = TFBertForSequenceClassification.from_pretrained(args.pretrained, config=bert_config)
-    # else:
-    if args.model is not None:
-        model = tf.keras.models.load_model(args.model)
-    elif args.ckpt is not None:
-        # load model with checkpoint
-        # if args.model_type == 'BERT_HUGGINGFACE':
-        #     # create BERT config object + model
-        #     bert_config = BertConfig(vocab_size=args.config_dict["vocab_size"])
-        #     model = TFBertForSequenceClassification(config=bert_config)            
-        # else:
-        model = models[args.model_type](args, args.vector_size, args.embedding_size, num_labels, vocab_size, args.dropout_rate)
-               
-        # the following 2 restore() functions and load_weights function raise warnings about inconsistent references, the model is not correctly loaded
-        # for bert at least
-        checkpoint = tf.train.Checkpoint(optimizer=opt, model=model)
-        checkpoint.restore(args.ckpt).expect_partial()
-        # checkpoint.restore(args.ckpt) # same warning as checkpoint.restore(args.ckpt).expect_partial()
-        # model.load_weights(args.ckpt) # does not work
+        model = TFBertForSequenceClassification.from_pretrained(args.pretrained, config=bert_config)
+    else:
+        if args.model is not None:
+            model = tf.keras.models.load_model(args.model)
+        elif args.ckpt is not None:
+            # load model with checkpoint
+            # if args.model_type == 'BERT_HUGGINGFACE':
+            #     # create BERT config object + model
+            #     bert_config = BertConfig(vocab_size=args.config_dict["vocab_size"])
+            #     model = TFBertForSequenceClassification(config=bert_config)            
+            # else:
+            model = models[args.model_type](args, args.vector_size, args.embedding_size, num_labels, vocab_size, args.dropout_rate)
+                   
+            # the following 2 restore() functions and load_weights function raise warnings about inconsistent references, the model is not correctly loaded
+            # for bert at least
+            checkpoint = tf.train.Checkpoint(optimizer=opt, model=model)
+            checkpoint.restore(args.ckpt).expect_partial()
+            # checkpoint.restore(args.ckpt) # same warning as checkpoint.restore(args.ckpt).expect_partial()
+            # model.load_weights(args.ckpt) # does not work
 
     # define metrics
     if args.data_type == 'sim':
