8ea509cce6cde5a0c369c1941dbe3aab0e06b661
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2025-01-20 10:01:37 -0500

    parse fq file for dltoda models

--- DL_scripts/create_tfrecords.py
+++ DL_scripts/create_tfrecords.py
@@ -261,7 +261,7 @@ def create_tfrecords(args):
                     # label = ""
                     for line in f:
                         if line_count == 1:
-                            print(line)
+                            print(line.rstrip().split('|'))
                             label = line.rstrip().split('|')[1]
                         elif line_count == 2:
                             dna_sequence = line.rstrip()
