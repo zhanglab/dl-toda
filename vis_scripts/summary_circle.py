3a042dd2ad15652e04e71f7783b8bd65111a62f9
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2025-01-21 09:07:07 -0500

    add info about fp

--- vis_scripts/summary_circle.py
+++ vis_scripts/summary_circle.py
@@ -4,7 +4,7 @@ import glob
 import argparse
 import statistics
 from collections import defaultdict
-# from pycirclize import Circos
+from pycirclize import Circos
 sys.path.append('/'.join(os.path.dirname(os.path.abspath(__file__)).split('/')[:-1]))
 from dataprep_scripts.utils import load_fq_file
 from vis_scripts.parse_samfile import LoadData, GetCoverageOfSample
