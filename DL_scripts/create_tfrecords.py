508e03c4afb781123e0e94b9b654a35eb7753604
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2025-01-30 11:13:50 -0500

    process art data for cnn from tsv files

--- DL_scripts/create_tfrecords.py
+++ DL_scripts/create_tfrecords.py
@@ -246,8 +246,7 @@ def create_tfrecords(args):
                         for token_id in dna_list:
                             for key, value in args.dict_kmers.items():
                                 if value == token_id:
-                                    if key == "]":
-                                        print(key, value)
+                                    print(token_id, value, key)
                                     reconstructed_token_list.append(key)
                         reconstructed_read = reconstructed_token_list[0]
                         for i in range(1, len(reconstructed_token_list), 1):
