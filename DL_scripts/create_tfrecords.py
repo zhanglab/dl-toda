7c1b057e01d6a588376c7402438f6070bda80faa
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2025-01-30 11:07:00 -0500

    process art data for cnn from tsv files

--- DL_scripts/create_tfrecords.py
+++ DL_scripts/create_tfrecords.py
@@ -257,9 +257,6 @@ def create_tfrecords(args):
                     if args.update_labels:
                         label = int(args.labels_mapping[label])
 
-                    print(f'line: {line}\nlabel: {label}\ndna sequence: {dna_sequence}\ninput ids: {dna_list}')
-
-                    break
                     # create TFrecords
                     if args.no_label:
                         tfrecord_data = \
