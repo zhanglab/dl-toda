48c845a5709f0ba3384c0096f07055f1c95390ae
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2025-02-02 12:28:35 -0500

    simplify tf data loading pipeline

--- DL_scripts/read_classifier_meta.py
+++ DL_scripts/read_classifier_meta.py
@@ -354,7 +354,7 @@ def main():
         
         for batch, data in enumerate(test_input.take(test_steps), 1):
             # batch_predictions, batch_pred_sp, batch_prob_sp = testing_step(args.data_type, reads, labels, model)
-            batch_pred_sp, batch_prob_sp = testing_step(args.data_type, reads, labels, model)
+            batch_pred_sp, batch_prob_sp = testing_step(args.model_type, data, model)
 
             if batch == 1:
                 all_labels = [labels]
