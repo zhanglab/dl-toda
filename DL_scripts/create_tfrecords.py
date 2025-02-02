e8aa4246d1095e9e10b2bf8e274ab285dd8e1473
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2025-02-02 08:28:42 -0500

    process long meta reads

--- DL_scripts/create_tfrecords.py
+++ DL_scripts/create_tfrecords.py
@@ -48,8 +48,10 @@ def create_meta_tfrecords(args):
                 if len(kmer_vector) > args.kmer_vector_length:
                     num_parts = math.ceil(len(kmer_vector) / args.kmer_vector_length)
                     grouped_tokens = [kmer_vector[i:i+args.kmer_vector_length] for i in range(0, num_parts, args.kmer_vector_length)]
-                    print(num_parts, len(grouped_tokens), grouped_tokens[0], len(grouped_tokens[0]))
+                    print(len(read), num_parts, len(grouped_tokens), grouped_tokens[0], len(grouped_tokens[0]))
+                    print(grouped_tokens)
                     break
+                    # prepare_data_for_bert(args, dna_list
 
                 # data = \
                 #     {
