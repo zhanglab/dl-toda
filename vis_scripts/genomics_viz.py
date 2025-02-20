bde0888e3e33755c75cdd91bb5b5a9dcef677db7
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2025-02-20 10:10:32 -0500

    plot fn and tp as lines

--- vis_scripts/genomics_viz.py
+++ vis_scripts/genomics_viz.py
@@ -42,7 +42,7 @@ def GetFNOtherInfo(args, pos_test_alignments, neg_train_alignments, annot_info):
 			list_reads_id.append(readid)
 			if readid in pos_test_alignments:
 				genes = []
-				for pos in range(data[1], data[2]+1, 1):
+				for pos in range(pos_test_alignments[readid][1], pos_test_alignments[readid][2]+1, 1):
 					for gene_id, annot in annot_info.items():
 						if pos >= annot[0] and pos <= annot[1]:
 							genes.append(annot[3])
