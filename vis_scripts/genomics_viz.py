4b96b49e9467965b16ad11a95c0994afdac737be
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2025-02-20 10:08:22 -0500

    plot fn and tp as lines

--- vis_scripts/genomics_viz.py
+++ vis_scripts/genomics_viz.py
@@ -41,11 +41,15 @@ def GetFNOtherInfo(args, pos_test_alignments, neg_train_alignments, annot_info):
 			taxa[data[0]] += 1
 			list_reads_id.append(readid)
 			if readid in pos_test_alignments:
-				gene = ''
-				for gene_id, annot in annot_info.items():
-					if pos >= annot[0] and pos <= annot[1]:
-						gene = annot[3]
-				f.write(f"{readid}\t{data[0]}\t{gene}\n")
+				genes = []
+				for pos in range(data[1], data[2]+1, 1):
+					for gene_id, annot in annot_info.items():
+						if pos >= annot[0] and pos <= annot[1]:
+							genes.append(annot[3])
+				f.write(f"{readid}\t{data[0]}")
+				for g in genes:
+					f.write(f'\t{g}')
+				f.write('\n')
 				
 	taxa_sorted = dict(sorted(taxa.items(), key=lambda item: item[1], reverse=True))
 	most_mapped_taxon = ''
