39a2db01c59d166fe2e45299577126a779248ab1
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2025-02-20 12:49:35 -0500

    plot fn and tp as lines

--- vis_scripts/genomics_viz.py
+++ vis_scripts/genomics_viz.py
@@ -470,7 +470,7 @@ if __name__ == "__main__":
 	
 	# get annotations info
 	pos_test_annot_info = GetAnnotInfo(args, args.test_genomes_info[args.label][0], input_dir)
-	fn_positions_seq_length, fn_genes_of_interest, fn_not_associated_w_genes = GetPosOfInterest(args, annot_info, fn_alignments_pos_test, sequence_length)
+	fn_positions_seq_length, fn_genes_of_interest, fn_not_associated_w_genes = GetPosOfInterest(args, pos_test_annot_info, fn_alignments_pos_test, sequence_length)
 	print(len(fn_genes_of_interest))
 
 	# get mapping of false negatives to training genomes from other species
