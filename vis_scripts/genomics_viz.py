e311ae1c3e25873802d30d1b5d1c39381a45b34e
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2025-02-20 12:48:30 -0500

    plot fn and tp as lines

--- vis_scripts/genomics_viz.py
+++ vis_scripts/genomics_viz.py
@@ -469,7 +469,7 @@ if __name__ == "__main__":
 	fn_alignments_pos_test = GetAlignmentsInfo(fn_sequences, args.pos_test_pos_test, sequence_length, seq_to_labels, os.path.join(args.output_dir, f'fn_pos_test_pos_test_{args.prob_threshold}_mapping_info.tsv'))
 	
 	# get annotations info
-	annot_info = GetAnnotInfo(args, args.test_genomes_info[args.label][0], input_dir)
+	pos_test_annot_info = GetAnnotInfo(args, args.test_genomes_info[args.label][0], input_dir)
 	fn_positions_seq_length, fn_genes_of_interest, fn_not_associated_w_genes = GetPosOfInterest(args, annot_info, fn_alignments_pos_test, sequence_length)
 	print(len(fn_genes_of_interest))
 
