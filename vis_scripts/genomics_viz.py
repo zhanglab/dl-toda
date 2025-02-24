97f1a9ee164beeb625d4a6e467b90fd9d7f72d66
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2025-02-24 11:21:09 -0500

    use blast instead of bowtie2

--- vis_scripts/genomics_viz.py
+++ vis_scripts/genomics_viz.py
@@ -211,6 +211,8 @@ def GetAnnotInfo(args, genome_id, input_dir):
 				e = e.replace('"', '')
 				# get all go_function entries and choose go_function with the most details
 				if 'go_function' in e:
+					print(e)
+					print(e.split(' ')[2])
 					if len(e.split(' ')[2].split('|')[0]) > len(function):
 						print(e.split(' ')[2].split('|')[0])
 						function = e.split(' ')[2].split('|')[0]
