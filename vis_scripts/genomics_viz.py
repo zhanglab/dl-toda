8335fa3211c8c19f7dbe5a3845e5d01ca3936ff0
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2025-02-24 13:34:23 -0500

    use blast instead of bowtie2

--- vis_scripts/genomics_viz.py
+++ vis_scripts/genomics_viz.py
@@ -501,7 +501,7 @@ def FNCircosPlot(args, record_id, record_seq, record_fasta, fn_alignments_pos_te
 		tp_track.axis(ec="blue")
 		pos_tp_count = [0]*target_fasta.full_genome_length
 		for readid, data in tp_alignments_pos_test.items():
-			for pos in range(data[1], data[2]+1, 1):
+			for pos in range(data[2], data[3]+1, 1):
 				print(pos, target_fasta.full_genome_length, readid)
 				pos_tp_count[pos-1] +=1
 		y_values = list(range(min(pos_tp_count), max(pos_tp_count), 3))
