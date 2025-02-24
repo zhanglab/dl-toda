d7e8aba5cadda598e83aa460242cd85c8da8447d
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2025-02-24 16:04:10 -0500

    use blast instead of bowtie2

--- vis_scripts/genomics_viz.py
+++ vis_scripts/genomics_viz.py
@@ -698,7 +698,7 @@ if __name__ == "__main__":
 
 	# do FP analysis
 	# blast FP reads
-	with open(os.path.join(args.output_dir, f'{args.label}_FP_reads.fna'), "w") as outf
+	with open(os.path.join(args.output_dir, f'{args.label}_FP_reads.fna'), "w") as outf:
 		for k, v in readid_to_read,.items():
 			if k in fp_sequences:
 				outf.write(f'>{k}\n{v}\n')
