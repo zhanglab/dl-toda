7df4327f040a8d9924480a52c6bf7c67faee0baa
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2025-02-24 07:14:17 -0500

    use blast instead of bowtie2

--- vis_scripts/genomics_viz.py
+++ vis_scripts/genomics_viz.py
@@ -278,12 +278,12 @@ def GetAlignmentsInfo(sequences, input_file, sequence_length, seq_to_labels, out
 	alignments = defaultdict(list)
 	with open(input_file, 'r') as f:
 		for line in f:
+			print(line)
 			readid = line.rstrip().split(',')[0]
 			if readid in sequences:
 				sstart = int(line.rstrip().split(',')[2])
 				ssend = int(line.rstrip().split(',')[3])
 				seq_id = line.rstrip().split(',')[1]
-				mapped_reads_id.append(read)
 				alignments[readid] = [seq_id, sstart, ssend]
 
 	if outfilename:
