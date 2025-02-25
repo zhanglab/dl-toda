df548253f97c71fbde0d8a2f536a195bf483880f
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2025-02-24 20:48:31 -0500

    run blast in parallel

--- vis_scripts/genomics_viz.py
+++ vis_scripts/genomics_viz.py
@@ -102,7 +102,7 @@ def LoadFnaFile(fasta_file):
 
 def RunBlast(args, query, subject=None, db=False):
 	if db:
-		result = subprocess.run(['cat', query, '|', parallel_exec, '-j', f'{args.num_processes}', blastn_exec, '-query', f'{query}', '-db', '/datasets/bio/ncbi-db/2025-01-26/nt', '-out', f'{args.output_dir}/mapping/test_fp_blastn.out',
+		result = subprocess.run(['cat', query, '|', parallel_exec, '-j', f'{args.num_processes}', blastn_exec, '-query', '{}', '-db', '/datasets/bio/ncbi-db/2025-01-26/nt', '-out', f'{args.output_dir}/mapping/test_fp_blastn.out',
 		 '-outfmt', "10 delim=, qseqid sseqid evalue pident qseq sseq length ssciname stitle", '-max_target_seqs', '5', '-num_threads', '1'])
 
 		# result = subprocess.run([blastn_exec, '-query', f'{query}', '-db', '/datasets/bio/ncbi-db/2025-01-26/nt', '-out', f'{args.output_dir}/mapping/test_fp_blastn.out',
