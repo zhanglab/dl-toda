92dece18afdbad5dda4eac794d62488314e5ff2f
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2025-02-24 21:56:51 -0500

    modify blast command to use it in a bash script

--- vis_scripts/genomics_viz.py
+++ vis_scripts/genomics_viz.py
@@ -102,7 +102,7 @@ def LoadFnaFile(fasta_file):
 
 def RunBlast(args, query, subject=None, db=False):
 	if db:
-		sys.executable = blast_exec
+		sys.executable = blastn_exec
 		process = subprocess.run([sys.executable, '-query', f'{query}', '-db', '/datasets/bio/ncbi-db/2025-01-26/nt', '-out', 'blast_test.out',
 		 '-outfmt', "10 delim=, qseqid sseqid evalue pident qseq sseq length ssciname stitle", '-max_target_seqs', '5', '-num_threads', '1'], shell=True, check=True)
 		# process = subprocess.Popen(f'cat query | parallel_exec --colsep "," -j {args.num_processes} {blastn_exec} -query {{2}} -db /datasets/bio/ncbi-db/2025-01-26/nt -out {{1}} -outfmt 10 delim=, qseqid sseqid evalue pident qseq sseq length ssciname stitle -max_target_seqs 5 -num_threads 1', shell=True, executable="/bin/bash")
