6ddc7578f6a65a4c0fb0a6614523477ebf9a20e2
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2025-02-27 13:46:24 -0500

    add train coverage

--- vis_scripts/genomics_viz.py
+++ vis_scripts/genomics_viz.py
@@ -46,13 +46,14 @@ def GetGIs(args, annot_info, training_seq, testing_fasta):
 	# parse annotation information
 	annot_parsed = defaultdict(list)
 	for gene_id, data in annot_info.items():
-		print(data)
 		if data[0] == 'protein_coding':
 			if data[7] != '':
-				annot_parsed[data[7]] = [data[6], data[1], data[2], data[0]] # new locus tag, begin, end, gene type
+				print(data)
+				# annot_parsed[data[7]] = [data[6], data[1], data[2], data[0]] # new locus tag, begin, end, gene type
 		elif data[0] == 'rRNA' or data[0] == 'tRNA':
 			if data[6] != '':
-				annot_parsed[data[6]] = [data[5], data[1], data[2], data[0]]
+				print(data)
+				# annot_parsed[data[6]] = [data[5], data[1], data[2], data[0]]
 	# print(annot_parsed)
 	gis_info = defaultdict(list)
 	with open(args.genomic_islands, 'r') as f:
