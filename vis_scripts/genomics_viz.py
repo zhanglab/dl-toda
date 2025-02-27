284c41f765769559a59c518c8618452dda16bf2d
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2025-02-27 14:22:37 -0500

    add train coverage

--- vis_scripts/genomics_viz.py
+++ vis_scripts/genomics_viz.py
@@ -71,7 +71,6 @@ def GetGIs(args, training_seq, testing_fasta, input_dir):
 					gi_sequence = training_seq[start_locus_tag_start:end_locus_tag_end+1]
 				else:
 					gi_sequence = training_seq[end_locus_tag_end:start_locus_tag_start+1]
-				print(gi_sequence)
 				gis_info[gi_id] = [start_locus_tag_start, start_locus_tag_end, end_locus_tag_start, end_locus_tag_end]
 				fna.write(f'>{gi_id}\n{gi_sequence}\n')
 				outf.write(f'{gi_id}\t{start_locus_tag}\t{start_new_locus_tag}\t{start_locus_tag_start}\t{start_locus_tag_end}\t{end_locus_tag}\t{end_new_locus_tag}\t{end_locus_tag_start}\t{end_locus_tag_end}\t{start_locus_strand}\t{end_locus_strand}\n')
