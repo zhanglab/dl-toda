58473d8284e1cd1a12e6fe2af58317f7e8a39c2d
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2025-02-28 10:11:28 -0500

    add train coverage

--- vis_scripts/genomics_viz.py
+++ vis_scripts/genomics_viz.py
@@ -303,7 +303,7 @@ def RunBlast(args, output_dir, query, subject=None, db=False, outfilename=None, 
 			 	'-outfmt', "17", '-max_target_seqs', '5', '-num_threads', f'{args.num_processes}'])
 		else:
 			result = subprocess.run([blastn_exec, '-query', f'{query}', '-db', f'{output_dir}/blastdb', '-out', f'{outfilename}', \
-			 '-outfmt', "10 delim=, qseqid sseqid sstart send qstart qend qlen evalue pident", \
+			 '-outfmt', "10 delim=, qseqid sseqid sstart send qstart qend qlen evalue pident frames", \
 			 '-max_target_seqs', '5', '-num_threads', f'{args.num_processes}'])
 
 
@@ -642,7 +642,7 @@ def FNCircosPlot(args, test_record_seq, train_record_seq, testing_fasta, trainin
 	# align_coords = Blast([query_fasta, ref_fasta]).run()
 	# align_coords = AlignCoord.filter(align_coords, identity_thr=MIN_IDENTITY)
 	# run blast 		
-	RunBlast(args, os.path.join(args.output_dir, 'blast', 'test_train_genomes'), testing_fasta, subject=[training_fasta], outfilename=f'{args.output_dir}/blast/test_train_genomes/test_train_genomes_blastn.out', sam=True)
+	RunBlast(args, os.path.join(args.output_dir, 'blast', 'test_train_genomes'), testing_fasta, subject=[training_fasta], outfilename=f'{args.output_dir}/blast/test_train_genomes/test_train_genomes_blastn.out')
 
 
 	# # color = ColorCycler()
