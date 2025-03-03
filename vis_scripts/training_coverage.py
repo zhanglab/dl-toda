4bbbae1986cf35ecdd842437cab8f4f2ca83a11b
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2025-03-03 15:26:53 -0500

    add code

--- vis_scripts/training_coverage.py
+++ vis_scripts/training_coverage.py
@@ -482,13 +482,11 @@ def CircosPlot(args, testing_fasta, training_fasta, train_coverage, outfigpath, 
 		color = ColorCycler()
 		comp_name2color[genome_id] = color
 		for sector in circos.sectors:
-			sector.add_track((min_r_pos-5, min_r_pos), r_pad_ratio=0.1)
-		for ac in align_coords:
-			print(ac)
-			track = circos.get_sector(ac[3]).tracks[-1]
-			rect_color = interpolate_color(color, v=ac[2], vmin=MIN_IDENTITY)
-			track.rect(ac[0], ac[1], color=rect_color)
-			percent_identity.append(ac[2])
+			track = sector.add_track((min_r_pos-5, min_r_pos), r_pad_ratio=0.1)
+			for ac in align_coords:
+				rect_color = interpolate_color(color, v=ac[2], vmin=MIN_IDENTITY)
+				track.rect(ac[0], ac[1], color=rect_color)
+				percent_identity.append(ac[2])
 		min_r_pos -= 5
 
 		# get stats on percentage identity
