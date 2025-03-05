30f1c2255434f6222c0f8e08cf9936588f6b02b0
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2025-03-04 21:27:55 -0500

    add hist plot for attention scores

--- DL_scripts/vis_attentions.py
+++ DL_scripts/vis_attentions.py
@@ -253,6 +253,7 @@ def main():
             with open(os.path.join(args.output_dir, f'stats_att_{len(df)}_{reads_id[batch]}.tsv'), 'w') as f:
                 f.write(f'{np.mean(df_values)}\t{np.median(df_values)}\t{min(df_values)}\t{max(df_values)}')
 
+            plt.figure(figsize=(12, 12))
             sn.histplot(data=df)
             plt.xlabel('Attention scores')
             plt.ylabel('Frequency')
