acf389366a230c0e6cb9eaa243db142864235b31
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2025-03-05 08:32:56 -0500

    add list of reads to analyze

--- DL_scripts/vis_attentions.py
+++ DL_scripts/vis_attentions.py
@@ -277,8 +277,7 @@ def main():
                     sn.heatmap(data=df, annot=False, xticklabels=False, yticklabels=False, cmap=palette) 
                 plt.savefig(os.path.join(args.output_dir, f'attention_weights_heatmap_{len(df)}_{reads_id[batch]}.png'))
                 plt.close()
-                break
-            break
+
         
             # if label == 0:
             #     attention_weights_label_0.append(df.values.flatten().tolist())
