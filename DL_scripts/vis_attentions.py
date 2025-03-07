3bb66cf9701f7ae291b08cd58c31cb791f26b161
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2025-03-06 19:26:26 -0500

    add attentions scores to plot

--- DL_scripts/vis_attentions.py
+++ DL_scripts/vis_attentions.py
@@ -448,7 +448,7 @@ def main():
     # add attention scores
     all_attention_scores = []
     for v in attentions_df.values():
-        all_attention_scores += v.flatten().tolist()
+        all_attention_scores += v.values.flatten().tolist()
     min_attention_score = min(all_attention_scores)
     color, inverted_color = "grey", "red"
     for idx, track in enumerate(gv.feature_tracks, 1):
