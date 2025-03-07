ecc3268ed9bca048af56a2880cea80c05968275a
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2025-03-07 17:29:56 -0500

    comment out import of blast

--- vis_scripts/false_negatives.py
+++ vis_scripts/false_negatives.py
@@ -542,10 +542,12 @@ def GetReadsForAttentions(args, tp_alignments_pos_test, fn_alignments_pos_test, 
 				(fn_start_pos < tp_end_pos and fn_end_pos > tp_start_pos) or \
 				(tp_start_pos < fn_start_pos and tp_end_pos > fn_end_pos) or \
 				(fn_start_pos < tp_start_pos and fn_end_pos > tp_end_pos):
-				if tp_strand == fn_strand and abs(len(test_readid_to_read[fn_readid])-len(test_readid_to_read[tp_readid])) < 200:
-					reads.append([tp_readid, f'{tp_readid}-tp-{tp_start_pos}-{tp_end_pos}', tp_strand, fn_readid, f'{fn_readid}-fn-{fn_start_pos}-{fn_end_pos}', fn_strand])
-					reads_id[tp_readid] = f'{tp_readid}-tp-{tp_start_pos}-{tp_end_pos}'
-					reads_id[fn_readid] = f'{fn_readid}-fn-{fn_start_pos}-{fn_end_pos}'
+				if tp_strand == fn_strand:
+					if abs(len(test_readid_to_read[fn_readid])-len(test_readid_to_read[tp_readid])) < 200:
+						print(abs(len(test_readid_to_read[fn_readid])-len(test_readid_to_read[tp_readid])))
+						reads.append([tp_readid, f'{tp_readid}-tp-{tp_start_pos}-{tp_end_pos}', tp_strand, fn_readid, f'{fn_readid}-fn-{fn_start_pos}-{fn_end_pos}', fn_strand])
+						reads_id[tp_readid] = f'{tp_readid}-tp-{tp_start_pos}-{tp_end_pos}'
+						reads_id[fn_readid] = f'{fn_readid}-fn-{fn_start_pos}-{fn_end_pos}'
 
 	tsv_file = open(os.path.join(args.output_dir, f'{args.label}_contiguous_fn_tp_reads.tsv'), 'w')
 	sum_file = open(os.path.join(args.output_dir, f'{args.label}_contiguous_fn_tp_id.tsv'), 'w')
