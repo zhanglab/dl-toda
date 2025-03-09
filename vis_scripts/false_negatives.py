7703789c123e09409f74297dbb971760a92c76eb
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2025-03-09 12:10:42 -0400

    compute shannon entropy scores

--- vis_scripts/false_negatives.py
+++ vis_scripts/false_negatives.py
@@ -695,8 +695,8 @@ def GetShanningScore(testing_records, tp_alignments, fn_alignments):
 
 			# compute shannon entropy
 			if (shannon_tp + shannon_fn) == 0:
-				print('equal to 0', num_tp, prob_tp, shannon_tp, num_fn, prob_fn, shannon_fn, shannon_entropy)
 				shannon_entropy = 0
+				print('equal to 0', num_tp, prob_tp, shannon_tp, num_fn, prob_fn, shannon_fn, shannon_entropy)
 			else:
 				shannon_entropy = -(shannon_tp + shannon_fn)
 				print(' NOT equal to 0', num_tp, prob_tp, shannon_tp, num_fn, prob_fn, shannon_fn, shannon_entropy)
