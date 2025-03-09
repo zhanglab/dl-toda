8d8be05f965af50fd411e485894885affc5272f7
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2025-03-09 14:48:11 -0400

    compute shannon entropy scores

--- vis_scripts/false_negatives.py
+++ vis_scripts/false_negatives.py
@@ -690,9 +690,8 @@ def GetScores(testing_records, tp_alignments, fn_alignments):
 
 		if num_tp+num_fn > 0:
 			ratio_fn = num_fn / (num_tp+num_fn)
-			print(f'ratio fn: {ratio_fn}\t{num_tp}\t{num_fn}')
 
-			if ratio_fn > 1:
+			if ratio_fn > 0.5:
 				scores.append(ratio_fn)
 				reads_kept += fn_reads
 
