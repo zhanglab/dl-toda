69829748ea8f693a7d0da039042f2ea19a9c6ea4
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2025-03-13 22:07:34 -0400

    write label to output file

--- dataprep_scripts/select_genomes.py
+++ dataprep_scripts/select_genomes.py
@@ -102,6 +102,7 @@ def main():
     with open(args.labels, 'r') as f:
         for label in f:
             label = label.rstrip()
+            line = f'{label}\t'
             species = dl_toda_tax[label].split(';')[0]
             genus = dl_toda_tax[label].split(';')[1]
             print(label, species, genus)
@@ -110,10 +111,10 @@ def main():
                     if genomes_id[i] not in used_genomes:
                         if ncbi_assembly_level[i] == "Complete Genome" and ncbi_genome_category[i] != "derived from metagenome" and ncbi_genome_category[i] != "derived from environmental_sample":
                             if gtdb_taxonomy[i].split(';')[-1].split('__')[1] == species:
-                                line = '1\t'
+                                line += '1\t'
                             else:
                                 if gtdb_taxonomy[i].split(';')[-2].split('__')[1] == genus:
-                                    line = '0\t'
+                                    line += '0\t'
                             line += f'{genomes_id[i]}\t{gtdb_taxonomy[i]}\t{ncbi_assembly_level[i]}\t{ncbi_genome_category[i]}\t{ncbi_genome_representation[i]}\t{gtdb_rep_genome[i]}\t'
                             if genomes_id[i] in genomes_to_fa:
                                 line += 'IN\n'
