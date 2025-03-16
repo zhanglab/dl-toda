277f19e92b0b43c880ea9e523267e41a18629e9c
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2025-03-16 15:44:15 -0400

    update packages to import

--- vis_scripts/get_cog_functional_category.py
+++ vis_scripts/get_cog_functional_category.py
@@ -19,7 +19,7 @@ def RunRPSBLAST(args, protein_id):
 	result = subprocess.run([f'{edirect_exec}/esearch', '-query', f'{protein_id}', '-db', \
 	 'protein', '|', f'{edirect_exec}/efetch', '-format', 'fasta', '>', f'{args.output_dir}/proteins_fasta/{protein_id}_fna'], shell=True)
 
-	result = subprocess.run([f'{rpsblast_exec}', '-query', f'{args.output_dir}/proteins_fasta/{protein_id}_fna', '-db', f'{cog_db}', '-out', f'{args.output_dir}/rpsblast_results/{protein_id}_out.tsv', \
+	result = subprocess.run([f'{rpsblast_exec}', '-query', f'{args.output_dir}/proteins_fasta/{protein_id}_fna', '-db', '/work/pi_yingzhang_uri_edu/ccres/COG-db/Cog', '-out', f'{args.output_dir}/rpsblast_results/{protein_id}_out.tsv', \
 	 f'{cog_db}', '-outfmt', '6', '-num_threads', f'{args.num_processes}'], shell=True)
 
 
