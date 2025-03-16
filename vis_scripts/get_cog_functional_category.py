49d0f180714ab89400b17936f3c1b4283bc2b0aa
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2025-03-16 15:36:36 -0400

    update packages to import

--- vis_scripts/get_cog_functional_category.py
+++ vis_scripts/get_cog_functional_category.py
@@ -65,7 +65,7 @@ if __name__ == "__main__":
 	cogfncat_dict = defaultdict(str)
 	with open(cogfncat, 'r') as f:
 		for line in f:
-			if not line.rstrip().split('\t')[0].isdigit():
+			if line.rstrip().split('\t')[0] not in ['1', '2', '3', '4']:
 				print('no', line)
 				cogfncat_dict[line.rstrip().split('\t')[0]] = line.rstrip().split('\t')[3]
 	
