218cfdacf5f0fff8800aa2c0517179891c696652
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2025-03-18 06:38:07 -0400

    add print

--- vis_scripts/get_cog_functional_category.py
+++ vis_scripts/get_cog_functional_category.py
@@ -35,12 +35,13 @@ def GetCOGFnCat(args, protein_id, cdd_to_cog_df, coglettertofn_dict, cogfncat_di
 		row = cdd_to_cog_df.index[cdd_to_cog_df.iloc[:,0]==np.int64(cdd_id)].tolist()
 		assert len(row) == 1, f'CDD ID {cdd_id} has not been found'
 		cog_id = cdd_to_cog_df.iloc[row[0],1]
-
+		print(cog_id)
 		# get COG functional letter
 		if cog_id not in coglettertofn_dict:
 			return 'Function unknown'
 		else:
 			cog_letter = coglettertofn_dict[cog_id]
+			print(cog_letter, cogfncat_dict[cog_letter])
 			if len(cog_letter) > 1:
 				# retrieve most important function
 				cog_letter = cog_letter[0]
