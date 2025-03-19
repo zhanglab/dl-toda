c191578115ed534f32b32a3f314b6fe1a7c2445b
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2025-03-19 16:24:54 -0400

    parse all files per label at once

--- vis_scripts/get_cog_functional_category.py
+++ vis_scripts/get_cog_functional_category.py
@@ -117,9 +117,10 @@ if __name__ == "__main__":
 	with open(protein_id_to_faa, 'r') as f:
 		while True:
 			chunk = f.read(4096)
-			for i in range(len(chunk)):
-				print(chunk[i])
-				protein_to_faa[chunk[i].rstrip().split('\t')[0]] = chunk[i].rstrip().split('\t')[1]
+			print(chunk)
+			# for i in range(len(chunk)):
+			# 	print(chunk[i])
+			# 	protein_to_faa[chunk[i].rstrip().split('\t')[0]] = chunk[i].rstrip().split('\t')[1]
 			if not chunk:
 				break
 
