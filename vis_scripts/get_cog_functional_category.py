8f02158546649833a05383521b9154df5342e537
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2025-03-19 16:26:58 -0400

    parse all files per label at once

--- vis_scripts/get_cog_functional_category.py
+++ vis_scripts/get_cog_functional_category.py
@@ -115,15 +115,16 @@ if __name__ == "__main__":
 	print('before: loading', datetime.datetime.now())
 	protein_to_faa = {}
 	with open(protein_id_to_faa, 'r') as f:
-		while True:
-			chunk = f.read(4096)
-			print(chunk)
-			break
+		for line in f:
+		# while True:
+		# 	chunk = f.read(4096)
+		# 	print(chunk)
+		# 	break
 			# for i in range(len(chunk)):
 			# 	print(chunk[i])
-			# 	protein_to_faa[chunk[i].rstrip().split('\t')[0]] = chunk[i].rstrip().split('\t')[1]
-			if not chunk:
-				break
+			protein_to_faa[line.rstrip().split('\t')[0]] = line.rstrip().split('\t')[1]
+			# if not chunk:
+			# 	break
 
 		# protein_to_faa = {line.rstrip().split('\t')[0]: line.rstrip().split('\t')[1] for line in f.readlines()}
 	print('after: loading', datetime.datetime.now())
