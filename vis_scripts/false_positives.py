5f146d95a9dba1d9d71f7156e65d0af7aacbccad
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2025-03-20 13:30:12 -0400

    change color for score track

--- vis_scripts/false_positives.py
+++ vis_scripts/false_positives.py
@@ -1049,11 +1049,11 @@ def CircosPlot(args, scores, test_record_seq, train_record_seq, testing_fasta, t
 		# add track for scores
 		min_r_pos -= 5
 		scores_track = sector.add_track((min_r_pos-10, min_r_pos), r_pad_ratio=0.1)
-		scores_track.axis(ec="darkorange")
+		scores_track.axis(ec="deeppink")
 		y_values = list(range(math.floor(min(scores)), math.ceil(max(scores))+1, 1))
 		y_labels = list(map(str, y_values))
 		scores_track.yticks(y_values, y_labels)
-		scores_track.line(genome_pos, scores, color="darkorange")
+		scores_track.line(genome_pos, scores, color="deeppink")
 		print(f'added score track')
 
 		# add track for TP reads
