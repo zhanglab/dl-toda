82c7062e4e618835ceb051766db93f734083616c
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2025-04-10 16:13:55 -0400

    add print

--- vis_scripts/pangenome_utils.py
+++ vis_scripts/pangenome_utils.py
@@ -691,6 +691,7 @@ def GetFPTNReads(args, test_ordered_reads_id, test_sequence_length, outfile_sum)
 		for count, line in enumerate(f):
 			prob = float(line.rstrip().split('\t')[2])
 			if prob >= args.prob_threshold:
+				print(test_ordered_reads_id[count].split('|')[1])
 				if test_ordered_reads_id[count].split('|')[1] == args.test_label:
 					if line.rstrip().split('\t')[0] == '0' and line.rstrip().split('\t')[1] == '1':
 						fp_sequences.add(test_ordered_reads_id[count])
