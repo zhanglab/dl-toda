693ec14956b40c3c873379431a72b8244f11a005
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2025-04-16 07:53:13 -0400

    add break

--- DL_scripts/vis_attentions.py
+++ DL_scripts/vis_attentions.py
@@ -231,7 +231,6 @@ def main():
         print(pred_labels)
         print(pred_probs)
         print(reads_id[batch])
-        print(data)
         # print number of attention layers
         # print(f'# attentions layers: {len(attentions)}')
         # print dimensions of the output of the last attention layer
