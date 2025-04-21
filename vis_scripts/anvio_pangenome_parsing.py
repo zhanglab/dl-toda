2087b7483f95eb5b2175594aec93579d21d3f674
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2025-04-21 16:30:09 -0400

    download protein fasta files from ncbi

--- vis_scripts/anvio_pangenome_parsing.py
+++ vis_scripts/anvio_pangenome_parsing.py
@@ -2,26 +2,43 @@ import os
 import sys
 import argparse
 import subprocess
+import zipfile
 
 blastn_exec = "/modules/uri_apps/software/BLAST+/2.15.0-gompi-2023a/bin/blastp"
 makeblastdb_exec = "/modules/uri_apps/software/BLAST+/2.15.0-gompi-2023a/bin/makeblastdb"
 
-def RunBlast(output_dir, query, num_processes, subject, outfilename):
+def RunBlast(args, genome_id, output_dir, query, num_processes, outfilename):
 	
 	if not os.path.isdir(output_dir):
 		os.makedirs(output_dir)
 
+	if f'{genome_id}' not in os.listdir(args.protein_db):
+		protein_output_dir = os.path.join(args.protein_db, f'{genome_id}')
+		os.makedirs(protein_output_dir)
+		os.chdir(protein_output_dir)
+		# download feature table in gtf if not present
+		result = subprocess.run([ncbi_datasets_exec, 'download', 'genome', 'accession', f'{genome_id}', '--include', 'protein'])
+		# unzip output folder
+		with zipfile.ZipFile('ncbi_dataset.zip', 'r') as zip_ref:
+			zip_ref.extractall(os.getcwd())
+		os.chdir(input_dir)
+	else:
+		print(f'{genome_id}\tdownload already done')
+
+	protein_fasta = os.path.join(args.protein_db, genome_id, 'ncbi_dataset', 'data', genome_id, 'protein.faa')
 	# create database
-	result = subprocess.run([makeblastdb_exec, '-in', f'{subject}', '-input_type', 'fasta', '-dbtype', 'nucl', '-out', f'{output_dir}/blastdb'])
+	result = subprocess.run([makeblastdb_exec, '-in', f'{protein_fasta}', '-input_type', 'fasta', '-dbtype', 'prot', '-out', f'{output_dir}/blastdb'])
 	
 	# align amino acid sequences to database or fasta file
 	result = subprocess.run([blastn_exec, '-query', f'{query}', '-db', f'{output_dir}/blastdb', '-out', f'{outfilename}', \
 	 '-outfmt', "10 delim=, qseqid sseqid sstart send qstart qend qlen evalue pident qseq sseq sstrand", \
 	 '-max_target_seqs', '5', '-num_threads', f'{num_processes}'])
 
+
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
 	parser.add_argument('--output_dir', type=str, help='path to output directory')
+	parser.add_argument('--protein_db', type=str, help='path to directory containing protein fasta file associated with genomes')
 	parser.add_argument('--num_processes', type=int, help='number of processes to run in parallel')
 	parser.add_argument('--anvio_output', type=str, help='parse output files from anvio')
 	parser.add_argument('--testing_fasta_files', type=str, help='path to file mapping genomes accession id to path to fasta file')
@@ -38,7 +55,7 @@ if __name__ == "__main__":
 	# create dictionary mapping genome accession id to path of fasta file
 	with open(args.testing_fasta_files, 'r') as f:
 		content = f.readlines()
-		genomes_to_fasta = {content[i].rstrip().split('\t')[0]: content[i].rstrip().split('\t')[1] for i in range(len(content))}
+		genomes = [content[i].rstrip().split('\t')[0] for i in range(len(content))]
 
 	# parse output of anvio
 	with open(args.anvio_output, 'r') as f:
@@ -50,11 +67,11 @@ if __name__ == "__main__":
 		assert len(aas_sequences) == len(id_sequences)
 		# correct genomes accession id
 		for i in range(len(genomes_sequences)):
-			for key, value in genomes_to_fasta.items():
-				if genomes_sequences[i] in key:
-					genomes_sequences[i] = key
+			for j in range(len(genomes)):
+				if genomes_sequences[i] in genomes[j]:
+					genomes_sequences[i] = genomes[j]
 
-	for genome, fasta in genomes_to_fasta.items():
+	for genome in genomes:
 		if genome in genomes_sequences:
 			ids = [id_sequences[i] for i in range(len(id_sequences)) if genomes_sequences[i] == genome]
 			sequences = [aas_sequences[i] for i in range(len(aas_sequences)) if genomes_sequences[i] == genome]
@@ -65,8 +82,8 @@ if __name__ == "__main__":
 					outf.write(f'>{ids[i]}\n{sequences[i]}\n')
 
 			# align amino acid sequences to genome
-			RunBlast(os.path.join(args.output_dir, anvio_output_type, 'blast', genome), os.path.join(args.output_dir, anvio_output_type, f'{genome}-anvio-{anvio_output_type}.fna'), args.num_processes, \
-				fasta, f'{args.output_dir}/{anvio_output_type}/blast/{genome}/blastn.out')
+			RunBlast(args, genome, os.path.join(args.output_dir, anvio_output_type, 'blast', genome), os.path.join(args.output_dir, anvio_output_type, f'{genome}-anvio-{anvio_output_type}.fna'), \
+				args.num_processes, f'{args.output_dir}/{anvio_output_type}/blast/{genome}/blastn.out')
 
 
 
