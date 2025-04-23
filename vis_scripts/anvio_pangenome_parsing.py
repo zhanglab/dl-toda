65e56fae0bb2a5bf069f1a0528610a3d8475bdb1
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2025-04-23 08:46:03 -0400

    update input file

--- vis_scripts/anvio_pangenome_parsing.py
+++ vis_scripts/anvio_pangenome_parsing.py
@@ -2,50 +2,40 @@ import os
 import sys
 import argparse
 import subprocess
-from collections import defaultdict
 import zipfile
 sys.path.append('/'.join(os.path.dirname(os.path.abspath(__file__)).split('/')[:-1]))
-from pangenome_utils import GetAlignments, GetAnnotInfo, CheckSeqInGene
+from pangenome_utils import GetAlignments, GetAnnotInfo
 
-blastn_exec = "/modules/uri_apps/software/BLAST+/2.15.0-gompi-2023a/bin/blastn"
+blastn_exec = "/modules/uri_apps/software/BLAST+/2.15.0-gompi-2023a/bin/blastp"
 makeblastdb_exec = "/modules/uri_apps/software/BLAST+/2.15.0-gompi-2023a/bin/makeblastdb"
 ncbi_datasets_exec = "/work/pi_yingzhang_uri_edu/ccres/tools/datasets"
 
-def RunBlast(args, input_fasta, output_dir, query, num_processes, outfilename):
+def RunBlast(args, genome_id, output_dir, query, num_processes, outfilename, input_dir):
 	
 	if not os.path.isdir(output_dir):
 		os.makedirs(output_dir)
 
-	# if f'{genome_id}' not in os.listdir(args.protein_db):
-	# 	protein_output_dir = os.path.join(args.protein_db, f'{genome_id}')
-	# 	os.makedirs(protein_output_dir)
-	# 	os.chdir(protein_output_dir)
-	# 	# download feature table in gtf if not present
-	# 	result = subprocess.run([ncbi_datasets_exec, 'download', 'genome', 'accession', f'{genome_id}', '--include', 'protein'])
-	# 	# unzip output folder
-	# 	with zipfile.ZipFile('ncbi_dataset.zip', 'r') as zip_ref:
-	# 		zip_ref.extractall(os.getcwd())
-	# 	os.chdir(input_dir)
-	# else:
-	# 	print(f'{genome_id}\tdownload already done')
+	if f'{genome_id}' not in os.listdir(args.protein_db):
+		protein_output_dir = os.path.join(args.protein_db, f'{genome_id}')
+		os.makedirs(protein_output_dir)
+		os.chdir(protein_output_dir)
+		# download feature table in gtf if not present
+		result = subprocess.run([ncbi_datasets_exec, 'download', 'genome', 'accession', f'{genome_id}', '--include', 'protein'])
+		# unzip output folder
+		with zipfile.ZipFile('ncbi_dataset.zip', 'r') as zip_ref:
+			zip_ref.extractall(os.getcwd())
+		os.chdir(input_dir)
+	else:
+		print(f'{genome_id}\tdownload already done')
 
-	# protein_fasta = os.path.join(args.protein_db, genome_id, 'ncbi_dataset', 'data', genome_id, 'protein.faa')
+	protein_fasta = os.path.join(args.protein_db, genome_id, 'ncbi_dataset', 'data', genome_id, 'protein.faa')
 	# create database
-	# result = subprocess.run([makeblastdb_exec, '-in', f'{protein_fasta}', '-input_type', 'fasta', '-dbtype', 'prot', '-out', f'{output_dir}/blastdb'])
+	result = subprocess.run([makeblastdb_exec, '-in', f'{protein_fasta}', '-input_type', 'fasta', '-dbtype', 'prot', '-out', f'{output_dir}/blastdb'])
 	
 	# align amino acid sequences to database or fasta file
-	# result = subprocess.run([blastn_exec, '-query', f'{query}', '-db', f'{output_dir}/blastdb', '-out', f'{outfilename}', \
-	#  '-outfmt', "10 delim=, qseqid sseqid sstart send qstart qend qlen evalue pident qseq sseq sstrand", \
-	#  '-max_target_seqs', '5', '-num_threads', f'{num_processes}'])
-
-	# create database
-	result = subprocess.run([makeblastdb_exec, '-in', f'{input_fasta}', '-input_type', 'fasta', '-dbtype', 'nucl', '-out', f'{output_dir}/blastdb'])
-		
-
-	# align dna sequence to genome
-	result = subprocess.run([blastn_exec, '-task', 'blastn', '-query', f'{query}', '-db', f'{output_dir}/blastdb', '-out', f'{outfilename}', \
-			 '-outfmt', "10 delim=, qseqid sseqid sstart send qstart qend qlen evalue pident qseq sseq sstrand", \
-			 '-num_threads', f'{num_processes}'])
+	result = subprocess.run([blastn_exec, '-query', f'{query}', '-db', f'{output_dir}/blastdb', '-out', f'{outfilename}', \
+	 '-outfmt', "10 delim=, qseqid sseqid sstart send qstart qend qlen evalue pident qseq sseq sstrand", \
+	 '-max_target_seqs', '5', '-num_threads', f'{num_processes}'])
 
 
 if __name__ == "__main__":
@@ -76,30 +66,29 @@ if __name__ == "__main__":
 	# create dictionary mapping genome accession id to path of fasta file
 	with open(args.testing_fasta_files, 'r') as f:
 		content = f.readlines()
-		genomes_to_fasta = {content[i].rstrip().split('\t')[0]: content[i].rstrip().split('\t')[1] for i in range(len(content))}
+		genomes = [content[i].rstrip().split('\t')[0] for i in range(len(content))]
 
 	# parse output of anvio
 	with open(args.anvio_output, 'r') as f:
 		content = f.readlines()
 		id_sequences = [content[i].rstrip()[1:] for i in range(0, len(content), 2)]
-		dna_sequences = [content[i].rstrip() for i in range(1, len(content), 2)]
+		print(id_sequences[:10])
+		aas_sequences = [content[i].rstrip() for i in range(1, len(content), 2)]
 		# sort sequences based on genome of origin
 		genomes_sequences = [id_sequences[i].split('|')[2].split(':')[1] for i in range(len(id_sequences))]
-		assert len(dna_sequences) == len(id_sequences)
+		assert len(aas_sequences) == len(id_sequences)
 		# correct genomes accession id
 		for i in range(len(genomes_sequences)):
-			for genome in genomes_to_fasta.keys():
-				if genomes_sequences[i] in genome:
-					genomes_sequences[i] = genome
+			for j in range(len(genomes)):
+				if genomes_sequences[i] in genomes[j]:
+					genomes_sequences[i] = genomes[j]
 
 	outf = open(os.path.join(args.output_dir, f'{anvio_output_type}-genes-id.tsv'), 'w')
-	for genome in genomes_to_fasta.keys():
+	for genome in genomes:
 		print(genome)
-		print(genomes_to_fasta[genome])
-		# retrieve dna sequences and sequences id
 		if genome in genomes_sequences:
 			ids = [id_sequences[i] for i in range(len(id_sequences)) if genomes_sequences[i] == genome]
-			sequences = [dna_sequences[i] for i in range(len(dna_sequences)) if genomes_sequences[i] == genome]
+			sequences = [aas_sequences[i] for i in range(len(aas_sequences)) if genomes_sequences[i] == genome]
 
 			# write sequences to fasta file
 			with open(os.path.join(args.output_dir, anvio_output_type, f'{genome}-anvio-{anvio_output_type}.fna'), 'w') as fna:
@@ -114,55 +103,27 @@ if __name__ == "__main__":
 						updated_sequence = sequences[i]
 					fna.write(f'>{ids[i]}\n{updated_sequence}\n')
 
-			# align dna sequences to genome
-			RunBlast(args, genomes_to_fasta[genome], os.path.join(args.output_dir, anvio_output_type, 'blast', genome), os.path.join(args.output_dir, anvio_output_type, f'{genome}-anvio-{anvio_output_type}.fna'), \
-				args.num_processes, f'{args.output_dir}/{anvio_output_type}/blast/{genome}/blastn.out')
+			# align amino acid sequences to genome
+			RunBlast(args, genome, os.path.join(args.output_dir, anvio_output_type, 'blast', genome), os.path.join(args.output_dir, anvio_output_type, f'{genome}-anvio-{anvio_output_type}.fna'), \
+				args.num_processes, f'{args.output_dir}/{anvio_output_type}/blast/{genome}/blastp.out', input_dir)
 
 			# parse alignment
-			alignments = GetAlignments(ids, f'{args.output_dir}/{anvio_output_type}/blast/{genome}/blastn.out')
-			print('# sequences', len(alignments))
+			alignments = GetAlignments(ids, f'{args.output_dir}/{anvio_output_type}/blast/{genome}/blastp.out')
 			# get annotations of genome
 			annot_info, _ = GetAnnotInfo(args, genome, input_dir)
-			print('# genes', len(annot_info))
-			# get genes id from annotations
-			seq_in_genes = defaultdict(list)
-			for seq_id, align_info in alignments.items():
-				if align_info[1] < align_info[2]:
-					seq_start_pos = align_info[1]
-					seq_end_pos = align_info[2]
+			# get genes id from proteins id
+			for seq_id in ids:
+				if seq_id in alignments:
+					protein_id = alignments[seq_id][0]
+					# get gene id
+					seq_gene_id = 'NA'
+					for gene_id in annot_info.keys():
+						if annot_info[gene_id][-1] == protein_id:
+							seq_gene_id = gene_id
+					outf.write(f'{seq_id}\t{protein_id}\t{seq_gene_id}\t{gene_category}\n')
 				else:
-					seq_start_pos = align_info[2]
-					seq_end_pos = align_info[1]
-
-				for gene_id, data in annot_info.items():
-					gene_start_pos = data[1]
-					gene_end_pos = data[2]
-				
-					length_mapped_seq = CheckSeqInGene(seq_start_pos, seq_end_pos, gene_start_pos, gene_end_pos)
-					if length_mapped_seq != 0:
-						seq_in_genes[seq_id].append(gene_id)
-			
-			for key, value in seq_in_genes.items():
-				if len(value) > 1:
-					print(key, value)
-					break
-			break
-
-
-				# outf.write()
-
-	# 		for seq_id in ids:
-	# 			if seq_id in alignments:
-	# 				protein_id = alignments[seq_id][0]
-	# 				# get gene id
-	# 				seq_gene_id = 'NA'
-	# 				for gene_id in annot_info.keys():
-	# 					if annot_info[gene_id][-1] == protein_id:
-	# 						seq_gene_id = gene_id
-	# 				outf.write(f'{seq_id}\t{protein_id}\t{seq_gene_id}\t{gene_category}\n')
-	# 			else:
-	# 				outf.write(f'{seq_id}\tNA\tNA\t{gene_category}\n')
-	# outf.close()
+					outf.write(f'{seq_id}\tNA\tNA\t{gene_category}\n')
+	outf.close()
 
 
 
--- vis_scripts/get_cog_functional_category.py
+++ vis_scripts/get_cog_functional_category.py
@@ -17,12 +17,10 @@ coglettertofn = "/work/pi_yingzhang_uri_edu/ccres/COG2024/cog-24.def.tab"
 cogfncat = "/work/pi_yingzhang_uri_edu/ccres/COG2024/cog-24.fun.tab"
 
 
-def RunRPSBLAST(args, genome_id):
-	if not os.path.isdir(os.path.join(args.rpsblast_db, genome_id)):
-		os.makedirs(os.path.join(args.rpsblast_db, genome_id))
-	protein_fasta = os.path.join(args.protein_db, genome_id, 'ncbi_dataset', 'data', genome_id, 'protein.faa')
-	print(protein_fasta)
-	result = subprocess.run([f'{rpsblast_exec}', '-query', f'{protein_fasta}', '-db', '/work/pi_yingzhang_uri_edu/ccres/COG-db/Cog', '-out', f'{args.rpsblast_db}/{genome_id}/rpsblast_out.tsv', \
+
+def RunRPSBLAST(args):
+	fasta_file = os.path.join(args.output_dir, 'proteins_fasta', f'ncbi_dataset/data/{args.genome_id}/protein.faa')
+	result = subprocess.run([f'{rpsblast_exec}', '-query', f'{fasta_file}', '-db', '/work/pi_yingzhang_uri_edu/ccres/COG-db/Cog', '-out', f'{args.output_dir}/rpsblast_results/rpsblast_out.tsv', \
 	 '-outfmt', '6 delim=, qseqid sseqid evalue pident', '-num_threads', f'{args.num_processes}'])
 
 
@@ -30,8 +28,8 @@ def GetCOGFnCat(args, cdd_to_cog_df, coglettertofn_dict, cogfncat_dict):
 
 	# get best hit and its CDD ID for each protein query
 	proteins_cdd = defaultdict(list)
-	if os.path.exists(f'{args.rpsblast_db}/{genome_id}/rpsblast_out.tsv') and os.path.getsize(f'{args.rpsblast_db}/{genome_id}/rpsblast_out.tsv') != 0:
-		with open(f'{args.rpsblast_db}/{genome_id}/rpsblast_out.tsv', 'r') as f:
+	if os.path.exists(f'{args.output_dir}/rpsblast_results/rpsblast_out.tsv') and os.path.getsize(f'{args.output_dir}/rpsblast_results/rpsblast_out.tsv') != 0:
+		with open(f'{args.output_dir}/rpsblast_results/rpsblast_out.tsv', 'r') as f:
 			for line in f:
 				protein_id = line.rstrip().split('\t')[0]
 				cdd_id = line.rstrip().split('\t')[1].split('|')[2]
@@ -70,30 +68,35 @@ def GetCOGFnCat(args, cdd_to_cog_df, coglettertofn_dict, cogfncat_dict):
 	return proteins_fn
 
 
-def GetProteins(args, genome_id, input_dir):
-	if f'{genome_id}' not in os.listdir(args.protein_db):
-		protein_output_dir = os.path.join(args.protein_db, f'{genome_id}')
-		os.makedirs(protein_output_dir)
-		os.chdir(protein_output_dir)
-		# download feature table in gtf if not present
-		result = subprocess.run([ncbi_datasets_exec, 'download', 'genome', 'accession', f'{genome_id}', '--include', 'protein'])
-		# unzip output folder
-		with zipfile.ZipFile('ncbi_dataset.zip', 'r') as zip_ref:
-			zip_ref.extractall(os.getcwd())
-		os.chdir(input_dir)
-	else:
-		print(f'{genome_id}\tdownload already done')
+def GetProteins(args):
+	os.chdir(os.path.join(args.output_dir, 'proteins_fasta'))
+	# download feature table in gtf if not present
+	result = subprocess.run([ncbi_datasets_exec, 'download', 'genome', 'accession', f'{args.genome_id}', '--include', 'protein'])
+	# unzip output folder
+	with zipfile.ZipFile('ncbi_dataset.zip', 'r') as zip_ref:
+		zip_ref.extractall(os.getcwd())
+	os.chdir(args.input_dir)
+
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
-	parser.add_argument('--input_file', type=str, help='input file with genomes id')
+	parser.add_argument('--input_dir', type=str, help='directory containing results obtained from running pangenome.py')
+	parser.add_argument('--genome_id', type=str, help='genome accession id')
+	parser.add_argument('--output_dir', type=str, help='path to output directory')
 	parser.add_argument('--num_processes', type=int, help='number of processes to run in parallel')
-	parser.add_argument('--protein_db', type=str, help='path to directory containing protein fasta file associated with genomes')
-	parser.add_argument('--rpsblast_db', type=str, help='path to directory containing protein fasta file associated with genomes')
 	args = parser.parse_args()
 
-	input_dir = os.getcwd()
+	# create output directories
+	if not os.path.isdir(args.output_dir):
+		os.makedirs(args.output_dir)
+	if not os.path.isdir(os.path.join(args.output_dir, 'proteins_fasta')):
+		os.makedirs(os.path.join(args.output_dir, 'proteins_fasta'))
+	if not os.path.isdir(os.path.join(args.output_dir, 'rpsblast_results')):
+		os.makedirs(os.path.join(args.output_dir, 'rpsblast_results'))
 
+	# get all input files
+	input_files = glob.glob(os.path.join(args.input_dir, '*correct_genes_0.9.tsv')) + glob.glob(os.path.join(args.input_dir, '*incorrect_genes_0.9.tsv'))
+	print(input_files, len(input_files))
 	# load required files
 	cdd_to_cog_df = pd.read_csv(cddtocog, sep='\t', header=None)
 	
@@ -111,46 +114,37 @@ if __name__ == "__main__":
 				else:
 					cogfncat_dict[line.rstrip().split('\t')[0]] = line.rstrip().split('\t')[2]
 	
-	with open(args.input_file, 'r') as f:
-		content = f.readlines()
-		list_genomes = list(set([content[i].rstrip().split('\t')[-1] for i in range(len(content))]))
+	# get proteins associated with genome
+	GetProteins(args)
 
-	outfilename = args.input_file.split('.')[0]
-	print(outfilename)
-	# outf = open(os.path.join(args.output_dir, ''))
-	# for genome_id in list_genomes:
-	# 	print(genome_id)
-	# 	# get proteins associated with genome
-	# 	GetProteins(args, genome_id, input_dir)
+	# search proteins against the conserved domain database (CDD) with rpsblast
+	RunRPSBLAST(args)
 
-	# 	# search proteins against the conserved domain database (CDD) with rpsblast
-	# 	RunRPSBLAST(args, genome_id)
+	# get COG function for each protein
+	proteins_fn = GetCOGFnCat(args, cdd_to_cog_df, coglettertofn_dict, cogfncat_dict)
 
-	# 	# get COG function for each protein
-	# 	proteins_fn = GetCOGFnCat(args, cdd_to_cog_df, coglettertofn_dict, cogfncat_dict)
-	# 	print(proteins_fn)
-
-		# for i in range(len(input_files)):
-		# 	print(input_files[i])
-		# 	if os.path.exists(f'{input_files[i][:-4]}-w-COG.tsv'):
-		# 		os.remove(f'{input_files[i][:-4]}-w-COG.tsv')
-		# 	with open(f'{input_files[i][:-4]}-w-COG.tsv', 'w') as outf:
-		# 		if '_'.join(input_files[i].split('/')[-1].split('_')[1:3]) in ['fp_shared', 'tp_unique']:
-		# 			index = 18
-		# 		else:
-		# 			index = 16
-		# 		print('_'.join(input_files[i].split('/')[-1].split('_')[1:3]), index)
-				
-		# 		with open(input_files[i], 'r') as inf:
-		# 			for line in inf:
-		# 				if line.rstrip().split('\t')[index] == 'protein_coding':
-		# 					protein_id = line.rstrip().split('\t')[-1]
-		# 					if protein_id in proteins_fn:
-		# 						outf.write(f'{line.rstrip()}\t{proteins_fn[protein_id]}\n')
-		# 					else:
-	# 						outf.write(f'{line.rstrip()}\tFunction unknown\n')
-	# 				else:
-	# 					molecule = line.rstrip().split('\t')[index]
-	# 					outf.write(f'{line.rstrip()}\t{molecule}\n')
+	for i in range(len(input_files)):
+		print(input_files[i])
+		if os.path.exists(f'{input_files[i][:-4]}-w-COG.tsv'):
+			os.remove(f'{input_files[i][:-4]}-w-COG.tsv')
+		with open(f'{input_files[i][:-4]}-w-COG.tsv', 'w') as outf:
+			# if '_'.join(input_files[i].split('/')[-1].split('_')[1:3]) in ['fp_shared', 'tp_unique']:
+			# 	index = 18
+			# else:
+			# 	index = 16
+			# print('_'.join(input_files[i].split('/')[-1].split('_')[1:3]), index)
+			
+			with open(input_files[i], 'r') as inf:
+				for line in inf:
+					if line.rstrip().split('\t')[1] == 'protein_coding':
+						protein_id = line.rstrip().split('\t')[7]
+						if protein_id in proteins_fn:
+							outf.write(f'{line.rstrip()}\t{proteins_fn[protein_id]}\n')
+						else:
+							outf.write(f'{line.rstrip()}\tFunction unknown\n')
+					else:
+						molecule = line.rstrip().split('\t')[1]
+						new_line = '\t'.join(line.rstrip().split('\t')[0:6]) + '\tNA\t' + '\t'.join(line.rstrip().split('\t')[6:])
+						outf.write(f'{new_line}\t{molecule}\n')
 
 			
