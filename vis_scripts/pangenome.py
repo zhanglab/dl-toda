855eea8ea47c439b33db53e369f20ea2d3a9717d
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2025-04-26 09:28:35 -0400

    write reads to file for attentions

--- vis_scripts/pangenome.py
+++ vis_scripts/pangenome.py
@@ -88,10 +88,10 @@ if __name__ == "__main__":
 	# get genes and functions associated with incorrect and correct predictions
 	scores, incorrect_genes, correct_genes, incorrect_reads_kept, correct_reads_kept = GetGenes(args, test_annot_info, incorrect_alignments, correct_alignments, test_sequence_length, test_readid_to_read, len(testing_records[0].seq), incorrect_cs, correct_cs)
 
-	GetReadsForAttentions(args, correct_alignments, incorrect_alignments, incorrect_reads_kept, correct_reads_kept, test_readid_to_read)
+	gene_to_incorrect_reads_kept, gene_to_correct_reads_kept = GetReadsForAttentions(args, correct_alignments, incorrect_alignments, incorrect_reads_kept, correct_reads_kept, test_readid_to_read)
 
 	avg_pct_identity, ani, test_strain, train_strain = CircosPlot(args, scores, testing_records[0].seq, training_records[0].seq, args.testing_fasta, args.training_fasta, incorrect_alignments, correct_alignments, \
-			 incorrect_genes, correct_genes, os.path.join(args.output_dir, f'{args.testing_genome}_{args.prob_threshold}_circos.png'))
+			 incorrect_genes, correct_genes, gene_to_incorrect_reads_kept, gene_to_correct_reads_kept, test_readid_to_read, os.path.join(args.output_dir, f'{args.testing_genome}_{args.prob_threshold}_circos.png'))
 
 	train_species = args.dl_toda_tax[args.train_label].split(';')[0]
 	train_genus = args.dl_toda_tax[args.train_label].split(';')[1]
