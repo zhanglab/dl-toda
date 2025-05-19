47f53404626a93959bddf1a311adb2946c6b91eb
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2025-05-19 13:12:17 -0400

    update command line for blastn

--- vis_scripts/pangenome_utils.py
+++ vis_scripts/pangenome_utils.py
@@ -142,7 +142,7 @@ def RunBlast(output_dir, query, num_processes, subject=None, db=False, outfilena
 			 	'-outfmt', "17", '-max_target_seqs', '1', '-num_threads', f'{num_processes}'])
 		else:
 			# task = 'megablast'
-			task = 'blasn'
+			task = 'blastn'
 			result = subprocess.run([blastn_exec, '-query', f'{query}', '-task', f'{task}', '-db', f'{output_dir}/blastdb', '-out', f'{outfilename}', \
 			 '-outfmt', "10 delim=, qseqid sseqid sstart send qstart qend qlen evalue pident qseq sseq sstrand", \
 			 '-max_target_seqs', '5', '-num_threads', f'{num_processes}'])
