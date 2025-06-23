1d257fb7fecc7f3091a0034627dde9cd3b718212
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2025-06-23 18:42:37 -0400

    remove import of optimizers library

--- DL_scripts/read_classifier_training.py
+++ DL_scripts/read_classifier_training.py
@@ -23,7 +23,6 @@ from VDCNN import VDCNN
 from VGG16 import VGG16
 from DNA_model_1 import DNA_net_1
 from DNA_model_2 import DNA_net_2
-from optimizers import AdamWeightDecayOptimizer
 import argparse
 import random
 
