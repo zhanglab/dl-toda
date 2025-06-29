507a876483fd5b58970c02165bb0e33fc685246d
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2025-06-29 15:31:21 -0400

    add print

--- DL_scripts/read_classifier_meta.py
+++ DL_scripts/read_classifier_meta.py
@@ -353,6 +353,7 @@ def main():
         # all_prob_labels = [tf.zeros([args.batch_size], dtype=tf.dtypes.float32, name=None)]
         
         for batch, data in enumerate(test_input.take(test_steps), 1):
+            print('batch', batch)
             # batch_predictions, batch_pred_sp, batch_prob_sp = testing_step(args.data_type, reads, labels, model)
             batch_pred_sp, batch_prob_sp = testing_step(args.model_type, data, model)
 
@@ -366,7 +367,7 @@ def main():
                 all_pred_sp = tf.concat([all_pred_sp, [batch_pred_sp]], 1)
                 all_prob_sp = tf.concat([all_prob_sp, [batch_prob_sp]], 1)
                 # all_prob_labels = tf.concat([all_prob_labels, [batch_label_prob]], 1)
-            print('batch', batch)
+            
 
         # get list of true species, predicted species and predicted probabilities
         # all_predictions = all_predictions.numpy()
