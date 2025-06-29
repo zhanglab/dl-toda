7c4355f4826ee31a0779b6d27fdf21164e3cc8d4
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2025-06-29 15:23:46 -0400

    add print

--- DL_scripts/read_classifier_meta.py
+++ DL_scripts/read_classifier_meta.py
@@ -365,12 +365,14 @@ def main():
                 all_pred_sp = tf.concat([all_pred_sp, [batch_pred_sp]], 1)
                 all_prob_sp = tf.concat([all_prob_sp, [batch_prob_sp]], 1)
                 # all_prob_labels = tf.concat([all_prob_labels, [batch_label_prob]], 1)
+            print(batch)
 
         # get list of true species, predicted species and predicted probabilities
         # all_predictions = all_predictions.numpy()
         all_pred_sp = all_pred_sp[0].numpy()
         all_prob_sp = all_prob_sp[0].numpy()
         # all_prob_labels = all_prob_labels[0].numpy()
+        print(all_prob_sp)
         print(all_pred_sp)
         print(all_pred_sp.shape)
         print(f'before adjusting: {len(all_pred_sp)}\t{len(all_prob_sp)}\n')
