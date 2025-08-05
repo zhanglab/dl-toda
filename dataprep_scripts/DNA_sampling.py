f7184303ed510226f3e34da2c3c6c7fa91df9755
Cecile <cecile_cres@uri.edu>
2025-08-05 17:31:00 -0400

    add number of threads

--- dataprep_scripts/DNA_sampling.py
+++ dataprep_scripts/DNA_sampling.py
@@ -50,42 +50,42 @@ def PrepareContigsDb(args, genome_id):
     result = subprocess.run([os.path.join(anvio_exec_dir, 'anvi-run-hmms'), '--contigs-db', output_db, '--num-threads', f'{args.num_threads}'])
 
 def RunAnvio(args, genomes):
-    # Setup a COG data directory
-    result = subprocess.run([os.path.join(anvio_exec_dir, 'anvi-setup-ncbi-cogs')])
+    # # Setup a COG data directory
+    # result = subprocess.run([os.path.join(anvio_exec_dir, 'anvi-setup-ncbi-cogs')])
 
-    # Generate and annotate contigs databases
-    with mp.Manager() as manager:
-        processes = [mp.Process(target=PrepareContigsDb, args=(args, genomes[i])) for i in range(len(genomes))]
-        for p in processes:
-            p.start()
-        for p in processes:
-            p.join()
+    # # Generate and annotate contigs databases
+    # with mp.Manager() as manager:
+    #     processes = [mp.Process(target=PrepareContigsDb, args=(args, genomes[i])) for i in range(len(genomes))]
+    #     for p in processes:
+    #         p.start()
+    #     for p in processes:
+    #         p.join()
 
-    # # Create tsv file called genome_storage_input.txt
-    # with open(os.path.join(args.output_dir, 'anvio', 'genome_storage_input.txt'), 'w') as f:
-    #     f.write("name\tcontigs_db_path")
-    #     for genome_id in genomes:
-    #         genome_anvio_db = os.path.join(args.output_dir, 'anvio', f'{genome_id}_out.db')
-    #         f.write(f'{genome_id}\t{genome_anvio_db}\n')
+    # Create tsv file called genome_storage_input.txt
+    with open(os.path.join(args.output_dir, 'anvio', 'genome_storage_input.txt'), 'w') as f:
+        f.write("name\tcontigs_db_path")
+        for genome_id in genomes:
+            genome_anvio_db = os.path.join(args.output_dir, 'anvio', f'{genome_id}_out.db')
+            f.write(f'{genome_id}\t{genome_anvio_db}\n')
 
-    # # Generate a genomes storage
-    # out_genome_storage = os.path.join(args.output_dir, 'anvio', args.species.replace(" ", "") + '.db')
-    # print(out_genome_storage)
-    # result = subprocess.run([os.path.join(anvio_exec_dir, 'anvi-gen-genomes-storage'), '--external-genomes', 'genome_storage_input.txt', '--output-file', out_genome_storage])
+    # Generate a genomes storage
+    out_genome_storage = os.path.join(args.output_dir, 'anvio', args.species.replace(" ", "") + '.db')
+    print(out_genome_storage)
+    result = subprocess.run([os.path.join(anvio_exec_dir, 'anvi-gen-genomes-storage'), '--external-genomes', 'genome_storage_input.txt', '--output-file', out_genome_storage])
 
-    # # Run pangenome analysis using NCBI blastp for protein search
-    # blastp_out = os.path.join(args.output_dir, 'anvio', 'blastp')
-    # result = subprocess.run([os.path.join(anvio_exec_dir, 'anvi-pan-genome'), '--genomes-storage', out_genome_storage, '--project-name', args.species.replace(" ", ""), '--output-dir', blastp_out, '--num-threads', f'{args.num_threads}', '--use-ncbi-blast', '--mcl-inflation', '10'])
+    # Run pangenome analysis using NCBI blastp for protein search
+    blastp_out = os.path.join(args.output_dir, 'anvio', 'blastp')
+    result = subprocess.run([os.path.join(anvio_exec_dir, 'anvi-pan-genome'), '--genomes-storage', out_genome_storage, '--project-name', args.species.replace(" ", ""), '--output-dir', blastp_out, '--num-threads', f'{args.num_threads}', '--use-ncbi-blast', '--mcl-inflation', '10'])
 
-    # # Run pangenome analysis using DIAMOND for protein search
-    # blastp_out = os.path.join(args.output_dir, 'anvio', 'diamond')
-    # result = subprocess.run([os.path.join(anvio_exec_dir, 'anvi-pan-genome'), '--genomes-storage', out_genome_storage, '--project-name', args.species.replace(" ", ""), '--output-dir', blastp_out, '--num-threads', f'{args.num_threads}', '--mcl-inflation', '10', '--additional-params-for-seq-search', "--masking 0 --sensitive"])
+    # Run pangenome analysis using DIAMOND for protein search
+    blastp_out = os.path.join(args.output_dir, 'anvio', 'diamond')
+    result = subprocess.run([os.path.join(anvio_exec_dir, 'anvi-pan-genome'), '--genomes-storage', out_genome_storage, '--project-name', args.species.replace(" ", ""), '--output-dir', blastp_out, '--num-threads', f'{args.num_threads}', '--mcl-inflation', '10', '--additional-params-for-seq-search', "--masking 0 --sensitive"])
 
-    # # Retrieve singleton gene clusters
-    # blastp_out = os.path.join(args.output_dir, 'anvio', 'diamond')
-    # result = subprocess.run([os.path.join(anvio_exec_dir, 'anvi-get-sequences-for-gene-clusters'), '--pan-db', f'blastp_out/', '--project-name', args.species.replace(" ", ""), '--output-dir', blastp_out, '--num-threads', f'{args.num_threads}', '--mcl-inflation', '10', '--additional-params-for-seq-search', "--masking 0 --sensitive"])
+    # Retrieve singleton gene clusters
+    blastp_out = os.path.join(args.output_dir, 'anvio', 'diamond')
+    result = subprocess.run([os.path.join(anvio_exec_dir, 'anvi-get-sequences-for-gene-clusters'), '--pan-db', f'blastp_out/', '--project-name', args.species.replace(" ", ""), '--output-dir', blastp_out, '--num-threads', f'{args.num_threads}', '--mcl-inflation', '10', '--additional-params-for-seq-search', "--masking 0 --sensitive"])
 
-    # anvi-get-sequences-for-gene-clusters --pan-db label_$(echo $LABEL)_blastp_genomes/label_$(echo $LABEL)-PAN.db --genomes-storage label-$(echo $LABEL)-GENOMES.db --max-num-genomes 1 --max-num-genes-from-each-genome 1 --output-file label_$(echo $LABEL)_blastp_genomes/singleton-gene-clusters.fa
+    anvi-get-sequences-for-gene-clusters --pan-db label_$(echo $LABEL)_blastp_genomes/label_$(echo $LABEL)-PAN.db --genomes-storage label-$(echo $LABEL)-GENOMES.db --max-num-genomes 1 --max-num-genes-from-each-genome 1 --output-file label_$(echo $LABEL)_blastp_genomes/singleton-gene-clusters.fa
 
 def GetGenomes(args):
     genomes, ncbi_assembly_level, ncbi_genome_category, ncbi_genome_representation, gtdb_rep_genome, gtdb_taxonomy, ncbi_taxonomy = get_gtdb_info(args.gtdb_info)
