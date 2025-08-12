723fd7aac7b6252d116dead62ccc999f576fedd8
Cecile <cecile_cres@uri.edu>
2025-08-12 11:54:41 -0400

    create train and val data

--- dataprep_scripts/DNA_sampling.py
+++ dataprep_scripts/DNA_sampling.py
@@ -14,7 +14,7 @@ from pygenomeviz.parser import Fasta
 from Bio import SeqIO
 sys.path.append('/'.join(os.path.dirname(os.path.abspath(__file__)).split('/')[:-1]))
 print('/'.join(os.path.dirname(os.path.abspath(__file__)).split('/')[:-1]))
-break
+sys.exit(1)
 sys.path.append('/work/pi_yingzhang_uri_edu/ccres/tools/DNABERT/examples/data_process_template')
 from select_genomes import get_gtdb_info
 from process_pretrain_data import sampling, cut_no_overlap
