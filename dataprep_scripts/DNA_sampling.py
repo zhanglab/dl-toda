9149d6c457d87ebf2407f8b3b7a9833233d1d331
Cecile <cecile_cres@uri.edu>
2025-08-14 22:07:45 -0400

    correct vocabulary for cnn

--- dataprep_scripts/DNA_sampling.py
+++ dataprep_scripts/DNA_sampling.py
@@ -749,7 +749,8 @@ if __name__ == "__main__":
             # for cnn
             # get dictionary mapping kmers to indexes
             dict_kmers = vocab_dict(f'{args.vocab}/dltoda/{k_value}mers.txt')
-            with open(os.path.join(output_dir, f'{k_value}-dict.json'), 'w') as f:
+            print(dict_kmers)
+            with open(os.path.join(output_dir, 'cnn', f'{k_value}-dict.json'), 'w') as f:
                 json.dump(dict_kmers, f)
             create_tfrecords(input_file, os.path.join(output_dir, 'cnn'), k_value, args.step, args.max_read_length, kmer_vector_length, dict_kmers, labels_mapping, \
                 args.masked_lm_prob, dnabert=True, update_labels=True, bert_step=None, no_label=False, dataset_type='sim', bert=False)
