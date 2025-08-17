01b2b9c9f9ea164bdd0974bbe9835199de973efa
Cecile <cecile_cres@uri.edu>
2025-08-17 15:46:04 -0400

    add info about gene id

--- dataprep_scripts/DNA_sampling.py
+++ dataprep_scripts/DNA_sampling.py
@@ -810,7 +810,7 @@ if __name__ == "__main__":
                             outf_genes.write('correct\t')
                             correct += 1
                         probs.append(prob)
-                        outf_genes.write(f'{prob}\t')
+                        outf_genes.write(f'{prob}\n')
             accuracy = round(correct / (correct+incorrect), 2)
             outf.write(f'{genome_id}\t{accuracy}\t{correct}\t{incorrect}\t{statistics.median(probs)}\t{statistics.mean(probs)}\t{min(probs)}\t{max(probs)}\n')
         outf.close()
