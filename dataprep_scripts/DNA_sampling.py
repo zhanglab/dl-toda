04dc6b68b42b0536254a5af43f61fffa7dfd95a3
Cecile <cecile_cres@uri.edu>
2025-09-05 11:57:04 -0400

    download genomes

--- dataprep_scripts/DNA_sampling.py
+++ dataprep_scripts/DNA_sampling.py
@@ -483,8 +483,9 @@ def get_kmer_sentence(original_string, kmer=1, stride=1):
 def GetSequences(args, labels, num, sequences, info):
     for label in labels:
         genome_id = info[label]
-        print(genome_id)
-        print(genome_id in os.listdir(os.path.join(args.input_dir, 'ncbi_database')))
+        if genome_id not in os.listdir(os.path.join(args.input_dir, 'ncbi_database')):
+            GetGenomeAndAnnot(args, genome_id)
+            _ = PrepareFasta([genome_id])
         fasta = Fasta(glob.glob(os.path.join(args.input_dir, 'ncbi_database', genome_id, 'ncbi_dataset/data', genome_id, 'updated*.fna'))[0])
         starts, ends = sampling(length=fasta.full_genome_length, kmer=1, sampling_rate=0.5)
         sam_sequences = SampleGenome(starts, ends, fasta.full_genome_seq)
