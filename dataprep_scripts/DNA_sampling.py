ccc051bda0bf9ae47b0b913f1fab6de9d2e3def6
Cecile <cecile_cres@uri.edu>
2025-09-05 11:45:02 -0400

    update output format

--- dataprep_scripts/DNA_sampling.py
+++ dataprep_scripts/DNA_sampling.py
@@ -482,9 +482,7 @@ def get_kmer_sentence(original_string, kmer=1, stride=1):
 
 def GetSequences(args, labels, num, sequences, info):
     for label in labels:
-        genome_id = info[label][0]
-        print(genome_id)
-        print(os.path.join(args.input_dir, 'ncbi_database', genome_id, 'ncbi_dataset/data', genome_id, 'updated*.fna'))
+        genome_id = info[label]
         fasta = Fasta(glob.glob(os.path.join(args.input_dir, 'ncbi_database', genome_id, 'ncbi_dataset/data', genome_id, 'updated*.fna'))[0])
         starts, ends = sampling(length=int(info[label][2]), kmer=1, sampling_rate=0.5)
         sam_sequences = SampleGenome(starts, ends, fasta.full_genome_seq)
