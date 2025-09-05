ec069a0fb4202c571112a02ab85034bbb23c399d
Cecile <cecile_cres@uri.edu>
2025-09-05 12:05:45 -0400

    download genomes

--- dataprep_scripts/DNA_sampling.py
+++ dataprep_scripts/DNA_sampling.py
@@ -483,8 +483,6 @@ def get_kmer_sentence(original_string, kmer=1, stride=1):
 def GetSequences(args, labels, num, sequences, info):
     for label in labels:
         genome_id = info[label]
-        print(os.listdir(os.path.join(args.input_dir, 'ncbi_database'))[:10])
-        print(genome_id, genome_id in os.listdir(os.path.join(args.input_dir, 'ncbi_database')))
         if genome_id not in os.listdir(os.path.join(args.input_dir, 'ncbi_database')):
             print(f'genome {genome_id} not in database')
             GetGenomeAndAnnot(args, genome_id)
