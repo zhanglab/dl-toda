771d40ff36793987df3cc98f10ba61f728e91d38
Cecile <cecile_cres@uri.edu>
2025-09-05 11:50:30 -0400

    update output format

--- dataprep_scripts/DNA_sampling.py
+++ dataprep_scripts/DNA_sampling.py
@@ -484,10 +484,9 @@ def GetSequences(args, labels, num, sequences, info):
     for label in labels:
         genome_id = info[label]
         fasta = Fasta(glob.glob(os.path.join(args.input_dir, 'ncbi_database', genome_id, 'ncbi_dataset/data', genome_id, 'updated*.fna'))[0])
-        print(fasta.full_genome_seq, type(fasta.full_genome_seq))
-        starts, ends = sampling(length=fasta.full_genome_seq, kmer=1, sampling_rate=0.5)
+        starts, ends = sampling(length=fasta.full_genome_length, kmer=1, sampling_rate=0.5)
         sam_sequences = SampleGenome(starts, ends, fasta.full_genome_seq)
-        cuts = cut_no_overlap(length=fasta.full_genome_seq, kmer=1)
+        cuts = cut_no_overlap(length=fasta.full_genome_length, kmer=1)
         cut_sequences, seq_starts, seq_ends = CutGenome(cuts, fasta.full_genome_seq)
         all_sequences = sam_sequences + cut_sequences
         all_starts = starts + seq_starts
