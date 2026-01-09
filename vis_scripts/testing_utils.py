d4e6dc600c386fa008111296d7fb2f3a1f223bcd
Cecile <cecile_cres@uri.edu>
2026-01-09 12:04:39 -0500

    correct indentation error

--- vis_scripts/testing_utils.py
+++ vis_scripts/testing_utils.py
@@ -295,9 +295,9 @@ def GetGenes(annot_info, seq_start, seq_end):
 	for gene_id, data in annot_info.items():
 		gene_start_pos = data[1]
 		gene_end_pos = data[2]
-        if (seq_start < gene_start_pos and seq_end > gene_start_pos) or
-            (seq_start < gene_end_pos and seq_end > gene_end_pos) or 
-            (seq_start > gene_start_pos and seq_end < gene_end_pos) or 
+        if (seq_start < gene_start_pos and seq_end > gene_start_pos) or \
+            (seq_start < gene_end_pos and seq_end > gene_end_pos) or    \
+            (seq_start > gene_start_pos and seq_end < gene_end_pos) or  \
             (seq_start < gene_start_pos and seq_end > gene_end_pos):
             return data.insert(0, gene_id)
 
