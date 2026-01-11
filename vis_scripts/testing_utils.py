e579e560fb82c0b74fcf49c9c83eaae96d681b53
Cecile <cecile_cres@uri.edu>
2026-01-11 18:56:06 -0500

    write prob score of prediction only

--- vis_scripts/testing_utils.py
+++ vis_scripts/testing_utils.py
@@ -200,7 +200,7 @@ def CircosPlot(correct_seq, incorrect_seq, correct_genes, incorrect_genes, train
                     i_gene_num = len(incorrect_genes[gene_id])
                     i_gene_length = sum([seq[1] - seq[0] for seq in incorrect_genes[gene_id]])
 
-                gene_score = ((c_gene_length - i_gene_length)/(c_gene_length + i_gene_length))*100
+                gene_score = ((c_gene_length - i_gene_length)/(c_gene_length + i_gene_length))
                 print(gene_id, gene_score, c_gene_length, i_gene_length)
                 
                 pident_pos = []
