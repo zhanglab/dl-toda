3247dc2bea4798dfb087fa4a0031bdb24456ce71
Cecile <cecile_cres@uri.edu>
2026-01-11 17:44:38 -0500

    write prob score of prediction only

--- DL_scripts/read_classifier_pytorch.py
+++ DL_scripts/read_classifier_pytorch.py
@@ -428,7 +428,7 @@ if __name__ == "__main__":
             for k, v in correct_genes.items():
                 print(k, len(v), v)
             print('incorrect and correct genes', len(set(c_genes).intersection(set(i_genes))))
-            CircosPlot(correct_seq, incorrect_seq, correct_genes, incorrect_genes, args.train_fasta, args.test_fasta, args.test_genome_id, args.output_dir, args.num_processes):
+            CircosPlot(correct_seq, incorrect_seq, correct_genes, incorrect_genes, args.train_fasta, args.test_fasta, args.test_genome_id, args.output_dir, args.num_processes)
 
 
     if args.lc_dir is not None:
