1786381c62228324f68580dca295d1e622b1ebad
Cecile <cecile_cres@uri.edu>
2026-01-26 11:35:22 -0500

    replace unknown token

--- DL_scripts/read_classifier_pytorch.py
+++ DL_scripts/read_classifier_pytorch.py
@@ -458,7 +458,7 @@ if __name__ == "__main__":
                                 else:
                                     batch_seq += dict_tokens[input_ids[i]][-1]
                         # update original sequence if unknown character
-                        original_seq_updated = ''.join([c if c in ['A','T','C','G'] else 'U' for c in test_sequences[batch][0]])
+                        original_seq_updated = ''.join([c if c in ['A','T','C','G'] else 'UUUU' for c in test_sequences[batch][0]])
                         print(original_seq_updated)
                         assert batch_seq == original_seq_updated, f'not the same sequence: {batch_seq}\t{original_seq_updated}\t{test_sequences[batch][0]}'
                         result = 'I' if batch_ground_truth[0] != batch_predictions[0] else 'C'
