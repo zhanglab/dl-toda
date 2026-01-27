c14fd6b039cd2202d3bd3d7b1db57811adce49af
Cecile <cecile_cres@uri.edu>
2026-01-27 12:03:22 -0500

    add interpretability mode

--- DL_scripts/read_classifier_pytorch.py
+++ DL_scripts/read_classifier_pytorch.py
@@ -439,9 +439,9 @@ if __name__ == "__main__":
             # update original sequence if presence of unknown character
             seq_updated = ''
             i = 0
-            while i < len(test_sequences[batch][0]):
-                if test_sequences[batch][0][i] in ['A','T','C','G']:
-                    seq_updated += test_sequences[batch][0][i]
+            while i < len(sequences[batch][0]):
+                if sequences[batch][0][i] in ['A','T','C','G']:
+                    seq_updated += sequences[batch][0][i]
                 else:
                     seq_updated += 'UUUU'
                     i += 3
