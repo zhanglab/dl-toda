f16baed68348698e6d79d3d72e3b4dd6beb01716
Cecile <cecile_cres@uri.edu>
2026-02-25 15:57:32 -0500

    process results in parallel

--- DL_scripts/read_classifier_pytorch.py
+++ DL_scripts/read_classifier_pytorch.py
@@ -560,8 +560,8 @@ if __name__ == "__main__":
             grouped_sequences_batch_idx = [batch_seq_idx[i:i+chunk_size] for i in range(0, len(batch_seq_idx), chunk_size)]
             prev_batch_size = len(batch_predictions)
             embeddings = outputs.hidden_states[-1].tolist()
-            attentions = list(outputs.attentions)
-            attentions = [i.tolist() for i in attentions]
+            attentions = outputs.attentions.tolist()
+            # attentions = [i.tolist() for i in attentions]
             # print(attentions[0])
             print(type(attentions[0]))
             sys.exit(1)
