4721e2629b21530fc4cbedff9bddf6ad06e8ec53
Cecile <cecile_cres@uri.edu>
2026-03-10 17:49:26 -0400

    add percentage identity with train genomes

--- dataprep_scripts/prepare_dnabert_datasets.py
+++ dataprep_scripts/prepare_dnabert_datasets.py
@@ -191,7 +191,7 @@ def main():
         print(genomes_size)
 
     
-    labels = args.neg_label + [args.pos_label]
+    labels = sorted(args.neg_label + [args.pos_label])
     input_sam_data = [i for i in sorted(glob.glob(f"{args.input_dir}/{args.dataset}_data_label_*/k{args.kmer}/data_sam_*_k{args.kmer}")) if 'seq' not in i.rstrip().split('/')[-1] and i.rstrip().split('/')[-3].split('_')[3] in labels]
     input_cut_data = [i for i in sorted(glob.glob(f"{args.input_dir}/{args.dataset}_data_label_*/k{args.kmer}/data_cut_*_k{args.kmer}")) if 'seq' not in i.rstrip().split('/')[-1] and i.rstrip().split('/')[-3].split('_')[3] in labels]
     
