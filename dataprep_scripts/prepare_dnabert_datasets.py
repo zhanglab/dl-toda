228ffaf5f16e701510a74b8a1e9a483fc7221808
Cecile <cecile_cres@uri.edu>
2026-03-10 17:23:25 -0400

    add percentage identity with train genomes

--- dataprep_scripts/prepare_dnabert_datasets.py
+++ dataprep_scripts/prepare_dnabert_datasets.py
@@ -137,6 +137,7 @@ def GetGenomeCov(data, train_genome_size, label, out_f, datatype):
     for i in range(len(data)):
         start = int(data[i].split('\t')[2])
         end = int(data[i].split('\t')[3])
+        print(data[i], start, end)
         for j in range(start, end, 1):
             data_cov[j] += 1
     pct_genome_covered = (sum([1 for v in data_cov.values() if v != 0])/train_genome_size)*100
