47af1251fcdb8eadc3f6a585c4a335279452a0c6
Cecile <cecile_cres@uri.edu>
2026-03-13 09:42:46 -0400

    add percentage identity with train genomes

--- dataprep_scripts/prepare_dnabert_datasets.py
+++ dataprep_scripts/prepare_dnabert_datasets.py
@@ -23,6 +23,17 @@ blastn_exec = "/modules/uri_apps/software/BLAST+/2.15.0-gompi-2023a/bin/blastn"
 makeblastdb_exec = "/modules/uri_apps/software/BLAST+/2.15.0-gompi-2023a/bin/makeblastdb"
 ncbi_datasets_exec = "/work/pi_yingzhang_uri_edu/ccres/tools/datasets"
 
+def AddAlignmentsInfo(args, label_seq, label, genomes_size, out_info):
+    # get average percentage identity with train positive genome
+    pos_dict_pident = GetMatchRegions(args, os.path.join(blastoutdir, args.pos_label, 'blastn.out'), identity_thr=MIN_IDENTITY, key='subject')
+    # get average percentage identity with train negative genome
+    neg_dict_pident = GetMatchRegions(args, os.path.join(blastoutdir, args.neg_label[0], 'blastn.out'), identity_thr=MIN_IDENTITY, key='subject')
+    # update sequences with average percentage identity with positive and negative train genomes
+    label_seq = AddPctIdentity(pos_dict_pident, label_seq)
+    label_seq = AddPctIdentity(neg_dict_pident, label_seq)
+    # get coverage of testing genome
+    GetGenomeCov(label_seq, genomes_size[label], label, out_info, 'test')
+    return label_seq
 
 def DownloadGenome(args, genome_id):
 	if f'{genome_id}' not in os.listdir(args.ncbi_db):
@@ -430,7 +441,7 @@ def main():
                 p.start()
             for p in processes:
                 p.join()
-            
+            print(genomes_size)
             # create BLAST database for testing genomes
             for g in [pos_test_genome, neg_test_genome]:
                 fasta = glob.glob(os.path.join(args.ncbi_db, f'{g}/ncbi_dataset/data/{g}/*.fna'))[0]
@@ -443,33 +454,23 @@ def main():
 
             # only for finetuning
             out_info = open(os.path.join(args.output_dir, f'{args.bert_step}_l{args.pos_label}_test_data_info_k{args.kmer}.tsv'), 'w')
-            # get average percentage identity with train positive genome
-            pos_dict_pident = GetMatchRegions(args, os.path.join(blastoutdir, args.pos_label, 'blastn.out'), identity_thr=MIN_IDENTITY, key='subject')
-            # get average percentage identity with train negative genome
-            neg_dict_pident = GetMatchRegions(args, os.path.join(blastoutdir, args.neg_label[0], 'blastn.out'), identity_thr=MIN_IDENTITY, key='subject')
-            # update positive sequences with average percentage identity with positive and negative train genomes
-            pos_label_seq = AddPctIdentity(pos_dict_pident, sequences[args.pos_label])
-            pos_label_seq = AddPctIdentity(neg_dict_pident, pos_label_seq)
-            # get coverage of positive testing genome
-            GetGenomeCov(pos_label_seq, test_genomes_size[args.pos_label], args.pos_label, out_info, 'test')
-            # get sequences from negative label
-            labels_other = [l for l in labels if l != args.pos_label]
-            print(f'# negative labels: {len(labels_other)}')
-            num_sp_labels = len(labels_other)
-            num = len(pos_label_seq)
-            num_seq_per_sp = [num // num_sp_labels + (1 if x < num % num_sp_labels else 0) for x in range (num_sp_labels)]
-            print(num, num_sp_labels, len(num_seq_per_sp), num_seq_per_sp[:3])
+            
+            # # get sequences from negative label
+            # labels_other = [l for l in labels if l != args.pos_label]
+            # print(f'# negative labels: {len(labels_other)}')
+            # num_sp_labels = len(labels_other)
+            num_seq = len(pos_label_seq)
+            # num_seq_per_sp = [num // num_sp_labels + (1 if x < num % num_sp_labels else 0) for x in range (num_sp_labels)]
+            # print(num, num_sp_labels, len(num_seq_per_sp), num_seq_per_sp[:3])
             with open(os.path.join(args.output_dir, f'{args.bert_step}_l{args.pos_label}_test_data_k{args.kmer}.tsv'), 'w') as out_f:
                 for i in range(len(labels)):
-                    if labels[i] == args.pos_label:
-                        # out_f.write(''.join(sequences[labels[i]]))
-                        out_f.write(''.join(pos_label_seq))
-                        out_info.write(f'{labels[i]}\t{len(sequences[labels[i]])}\n')
+                    if labels[i] != args.pos_label:
+                        label_seq = sequences[labels[i]][:num_seq]
                     else:
-                        num_seq = num_seq_per_sp.pop()
-                        l_sequences = sequences[labels[i]][:num_seq]
-                        out_f.write(''.join(l_sequences))
-                        out_info.write(f'{labels[i]}\t{len(l_sequences)}\n')
+                        label_seq = sequences[labels[i]]
+                    label_seq = AddAlignmentsInfo(args, label_seq, labels[i], genomes_size, out_info)
+                    out_f.write(''.join(label_seq))
+                    out_info.write(f'{labels[i]}\t{len(label_seq)}\n')
 
 
 if __name__ == "__main__":
