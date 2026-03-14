2d1d7a25ef7dce57fdb16e0df3a56a95fdcd47f9
Cecile <cecile_cres@uri.edu>
2026-03-14 12:53:54 -0400

    select genomes from gtdb

--- dataprep_scripts/prepare_dnabert_datasets.py
+++ dataprep_scripts/prepare_dnabert_datasets.py
@@ -201,8 +201,8 @@ def RunBlast(list_queries, list_labels, output_dir):
     print(list_queries)
     print(list_labels)
     for i in range(len(list_labels)):
-        if not os.path.isdir(os.path.join(output_dir, list_labels[i])):
-            os.makedirs(os.path.join(output_dir, list_labels[i]))
+        if not os.path.isdir(os.path.join(output_dir, f'{list_labels[i]}')):
+            os.makedirs(os.path.join(output_dir, f'{list_labels[i]}'))
         # compare target genome with genome of negative label (query)
         result = subprocess.run([blastn_exec, '-query', f'{list_queries[i]}', '-task', 'blastn', '-db', f'{output_dir}/blastdb', '-out', f'{output_dir}/{list_labels[i]}/blastn.out', \
 			 '-outfmt', "10 delim=, qseqid sseqid sstart send qstart qend qlen evalue pident qseq sseq sstrand", \
