3c42910b6f8b88bca15a6af154cc122659016f71
Cecile <cecile_cres@uri.edu>
2026-03-14 12:37:35 -0400

    select genomes from gtdb

--- dataprep_scripts/prepare_dnabert_datasets.py
+++ dataprep_scripts/prepare_dnabert_datasets.py
@@ -33,7 +33,7 @@ def SelectGenomes(args):
         if ncbi_genome_representation[idx] == 'full':
             if ncbi_genome_category[idx] == 'derived from single cell':
                 pos_train_genomes.append(idx)
-            else:
+            elif ncbi_genome_category[idx] == 'derived from metagenome':
                 pos_test_genomes.append(idx)
     neg_train_genomes = []
     neg_test_genomes = []
@@ -41,9 +41,10 @@ def SelectGenomes(args):
         if ncbi_genome_representation[idx] == 'full':
             if ncbi_genome_category[idx] == 'derived from single cell':
                 neg_train_genomes.append(idx)
-            else:
+            elif ncbi_genome_category[idx] == 'derived from metagenome':
                 neg_test_genomes.append(idx)
     assert len(pos_train_genomes) >= 2 and len(neg_train_genomes) >= 2, f'# genomes for {args.pos_species}: {len(pos_train_genomes)}\t# genomes for {args.neg_species}: {len(neg_train_genomes)}'
+    assert len(pos_test_genomes) >= 2 and len(neg_test_genomes) >= 2, f'# genomes for {args.pos_species}: {len(pos_test_genomes)}\t# genomes for {args.neg_species}: {len(neg_test_genomes)}'
     # with open(os.path.join(args.output_dir, 'train_genomes.tsv'), 'w') as f:
     #     random.shuffle(pos_train_genomes)
     #     random.shuffle(neg_train_genomes)
@@ -52,8 +53,6 @@ def SelectGenomes(args):
     with open(os.path.join(args.output_dir, 'test_genomes.tsv'), 'w') as f:
         random.shuffle(pos_test_genomes)
         random.shuffle(neg_test_genomes)
-        print(pos_test_genomes)
-        print(neg_test_genomes)
         f.write(f'{args.pos_species}\t{genomes[pos_test_genomes[0]][3:]}\t{genomes[pos_test_genomes[0]]}\t{ncbi_genome_category[pos_test_genomes[0]]}\t{ncbi_genome_representation[pos_test_genomes[0]]}\t{gtdb_rep_genome[pos_test_genomes[0]]}\t{gtdb_taxonomy[pos_test_genomes[0]]}\n')
         f.write(f'{args.neg_species}\t{genomes[neg_test_genomes[0]][3:]}\t{genomes[neg_test_genomes[0]]}\t{ncbi_genome_category[neg_test_genomes[0]]}\t{ncbi_genome_representation[neg_test_genomes[0]]}\t{gtdb_rep_genome[neg_test_genomes[0]]}\t{gtdb_taxonomy[neg_test_genomes[0]]}\n')
 
